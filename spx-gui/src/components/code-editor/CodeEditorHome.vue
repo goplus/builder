f726dea41c46ddea4666b3dae8c6afc39d4cc1f1
luoliwoshang <2643523683@qq.com>
2024-01-22 14:41:50 +0800

    feat(gui) abstract toolbox & change theme to light

--- spx-gui/src/components/code-editor/CodeEditorHome.vue
+++ spx-gui/src/components/code-editor/CodeEditorHome.vue
@@ -1,13 +1,24 @@
+<!--
+ * @Author: Zhang Zhi Yang
+ * @Date: 2024-01-19 17:54:18
+ * @LastEditors: Zhang Zhi Yang
+ * @LastEditTime: 2024-01-22 14:35:35
+ * @FilePath: /builder/spx-gui/src/components/code-editor/CodeEditorHome.vue
+ * @Description: 
+-->
 
 <template>
-    <!-- TODO: abstract component to page -->
     <n-layout has-sider style="height: 700px;">
+        <n-layout-sider :native-scrollbar="false" style="width: 300px">
+            <ToolBox />
+        </n-layout-sider>
         <n-layout-content>
-            <CodeEditor></CodeEditor>
+            <CodeEditor />
         </n-layout-content>
     </n-layout>
 </template>
-<script setup>
+<script lang="ts" setup>
 import { NLayout, NLayoutSider, NLayoutContent } from "naive-ui";
 import CodeEditor from "./CodeEditor.vue"
+import ToolBox from "./ToolBox.vue"
 </script>
--- spx-gui/src/plugins/code-editor/config.ts
+++ spx-gui/src/plugins/code-editor/config.ts
@@ -2,7 +2,7 @@
  * @Author: Zhang Zhi Yang
  * @Date: 2024-01-16 10:59:32
  * @LastEditors: Zhang Zhi Yang
- * @LastEditTime: 2024-01-19 16:45:05
+ * @LastEditTime: 2024-01-22 11:28:58
  * @FilePath: /builder/spx-gui/src/plugins/code-editor/config.ts
  * @Description: 
  */
@@ -498,7 +498,7 @@ const brackets = [
 // editor options
 export const options: monaco.editor.IStandaloneEditorConstructionOptions = {
     language: 'spx', // define the language mode
-    theme: 'vs-dark', // choose vs, hc-black, or vs-dark
+    theme: 'vs', // choose vs, hc-black, or vs-dark
     selectOnLineNumbers: true, // select the line number's of the code
     roundedSelection: true, // rounded selection
     readOnly: false, // read/write
@@ -595,5 +595,10 @@ export const LanguageConfig: monaco.languages.LanguageConfiguration = {
 }
 export {
     keywords, typeKeywords, operators, brackets,
-    event_fn_completions, function_completions
+    function_completions,
+    motion_fn_completions,
+    event_fn_completions,
+    control_fn_completions,
+    sound_fn_completions,
+    look_fn_completions
 }
