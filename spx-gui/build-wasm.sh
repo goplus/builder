6fd13e491913ab98b4daf592ee79d0d81c703fd6
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-01-25 13:51:47 +0800

    add builder script

---
+++ spx-gui/build-wasm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo Run this script from 'spx-gui' directory
+
+cd ../tools/fmt
+source build.sh
+
+# cd ../ispx
+# source build.sh
+
+cd ..
+
+mkdir -p ../spx-gui/public/wasm
+cp fmt/static/main.wasm ../spx-gui/public/wasm/format.wasm
