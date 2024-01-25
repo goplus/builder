b79e3f06ce89dbae7864c3d7a23fa77823d8f33b
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-01-26 01:34:49 +0800

    update build-wasm script

--- spx-gui/build-wasm.sh
+++ spx-gui/build-wasm.sh
@@ -4,10 +4,11 @@ echo Run this script from 'spx-gui' directory
 cd ../tools/fmt
 source build.sh
 
-# cd ../ispx
-# source build.sh
+cd ../ispx
+source build.sh
 
 cd ..
 
 mkdir -p ../spx-gui/public/wasm
 cp fmt/static/main.wasm ../spx-gui/public/wasm/format.wasm
+cp ispx/main.wasm ../spx-gui/public/wasm/run.wasm
