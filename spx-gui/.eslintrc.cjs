435368c86ec8a0147371ae47c53d440ded35aa12
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-01-31 21:41:17 +0800

    update eslint config

--- spx-gui/.eslintrc.cjs
+++ spx-gui/.eslintrc.cjs
@@ -1,19 +1,18 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    // "plugin:@typescript-eslint/recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
-  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: "latest",
   },
   rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
     "no-useless-escape": "warn",
   },
 };
