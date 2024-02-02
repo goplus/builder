0458931b93ba583995fa78e571b1d44e574ed1dc
luoliwoshang <2643523683@qq.com>
2024-02-02 11:29:08 +0800

    chore:fix style's conflict

--- spx-gui/src/axios/index.ts
+++ spx-gui/src/axios/index.ts
@@ -2,14 +2,13 @@
  * @Author: Yao Xinyue
  * @Date: 2024-01-22 11:17:08
  * @LastEditors: Zhang Zhi Yang
- * @LastEditTime: 2024-02-02 11:03:51
+ * @LastEditTime: 2024-02-02 11:12:12
  * @FilePath: /builder/spx-gui/src/axios/index.ts
  * @Description:
  */
 import { createDiscreteApi } from "naive-ui";
 import axios, { type AxiosResponse } from "axios";
 
-const baseURL = "http://116.62.66.126:8081";
 const baseURL = "http://116.62.66.126:8081";
 
 const service = axios.create({
