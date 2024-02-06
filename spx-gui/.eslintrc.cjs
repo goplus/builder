f79c79c735284a41030ee9d5434f6ae7341729ca
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-02-05 20:05:58 -0800

    fix error on `npm run lint` & format (#71)

--- spx-gui/.eslintrc.cjs
+++ spx-gui/.eslintrc.cjs
@@ -1,19 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    // "plugin:@typescript-eslint/recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier/skip-formatting",
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest'
   },
   rules: {
-    "no-useless-escape": "warn",
-    "@typescript-eslint/naming-convention": "warn",
-  },
-};
+    'no-useless-escape': 'warn',
+    '@typescript-eslint/naming-convention': 'warn'
+  }
+}
