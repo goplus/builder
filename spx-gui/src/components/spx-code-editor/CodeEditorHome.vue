700738ac22e86d64775aba14f208011a4386d213
xuning <453594138@qq.com>
2024-02-07 13:50:27 +0700

    chore: style of top-menu and code eidtor

--- spx-gui/src/components/spx-code-editor/CodeEditorHome.vue
+++ spx-gui/src/components/spx-code-editor/CodeEditorHome.vue
@@ -1,20 +1,20 @@
 <!--
  * @Author: Zhang Zhi Yang
  * @Date: 2024-01-19 17:54:18
- * @LastEditors: Zhang Zhi Yang
- * @LastEditTime: 2024-02-02 14:24:10
+ * @LastEditors: xuning 453594138@qq.com
+ * @LastEditTime: 2024-02-06 20:56:03
  * @FilePath: /spx-gui/src/components/spx-code-editor/CodeEditorHome.vue
  * @Description: 
 -->
 
 <template>
-    <n-layout has-sider style="height: calc(100vh - 60px - 54px - 12px);">
+    <n-layout has-sider :style="layoutStyle">
         <n-layout-sider :native-scrollbar="false"
         collapse-mode="width"
         :collapsed-width="78"
         :width="260"
         show-trigger="arrow-circle"
-        style="background:rgba(255, 255, 255, 0)"
+        style="border-right: 2px dashed #8f98a1"
         >
             <ToolBox />
         </n-layout-sider>
@@ -28,4 +28,15 @@ import { NLayout, NLayoutSider, NLayoutContent } from "naive-ui";
 import SpxCodeEditor from "./SpxCodeEditor.vue"
 import ToolBox from "./ToolBox.vue"
 
+const layoutStyle = {
+    height: 'calc(100vh - 140px)',
+    display: 'flex',
+    flexDirection: 'column',
+    border: '2px solid #00142970',
+    position: 'relative',
+    background: 'white',
+    borderRadius: '0 24px 24px 0',
+    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
+    overflow: 'hidden',
+}
 </script>
