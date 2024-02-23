6680a90e73d7b6a002819cfcc80b6e18c9898c6a
wxy <1939311091@qq.com>
2024-02-23 17:10:46 +0800

    feat: encapsulate the functions that load、save the project and so on.

--- spx-gui/src/util/utils.ts
+++ spx-gui/src/util/utils.ts
@@ -1,6 +1,36 @@
 import * as qiniu from 'qiniu-js'
 import { service } from '@/axios'
 
+interface ProjectInfo {
+    id: string;
+    name: string;
+    version: number;
+}
+
+interface ProjectFiles {
+    [key: string]: string;
+}
+
+interface ProjectDetail {
+    authorId: string;
+    createdAt: string;
+    files: ProjectFiles;
+    id: string;
+    isPublic: number;
+    name: string;
+    status: number;
+    updatedAt: string;
+    version: number;
+}
+
+interface SaveProjectParams {
+    id?: string;
+    name: string;
+    uid: string;
+    isPublic: number;
+    files: ProjectFiles;
+}
+
 async function getUploadToken() {
     try {
         const response = await service.get('/project/upload-token')
@@ -35,4 +65,34 @@ async function uploadFile(file: File) {
     })
 }
 
-export { uploadFile }
+async function getProjectList(uid: string): Promise<ProjectInfo[]> {
+    try {
+        const response = await service.get(`/project/list/${uid}`)
+        return response.data.data
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
+
+async function loadProject(id: string): Promise<ProjectDetail> {
+    try {
+        const response = await service.get(`/project/detail/${id}`)
+        return response.data.data
+    } catch (error) {
+        console.error(error)
+        return <ProjectDetail>{}
+    }
+}
+
+async function saveProject(params: SaveProjectParams): Promise<ProjectDetail> {
+    try {
+        const response = await service.post('/project/save', params)
+        return response.data.data
+    } catch (error) {
+        console.error(error)
+        return <ProjectDetail>{}
+    }
+}
+
+export {uploadFile, getProjectList, loadProject, saveProject}
