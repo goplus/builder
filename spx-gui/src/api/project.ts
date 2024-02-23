9889c33c7e3ef0e73d6364d2f08df9b50b32e467
wxy <1939311091@qq.com>
2024-02-23 18:33:04 +0800

    feat: encapsulate the functions that load、save the project and so on. (#116)
    * feat: encapsulate the functions that load、save the project and so on.
    
    * feat: encapsulate the functions that load、save the project and so on.
    
    * format:

--- spx-gui/src/api/project.ts
+++ spx-gui/src/api/project.ts
@@ -1,33 +1,9 @@
-import type { Project } from "@/interface/library";
-import { service } from "@/axios"
+import type { ProjectDetail, ProjectInfo, SaveProjectParams } from "@/interface/library";
 import type { ResponseData } from "@/axios";
+import { service } from "@/axios"
 import type { FormatResponse } from "@/components/code-editor";
 import type { AxiosResponse } from "axios";
-
-/**
- * Saves a project.
- *
- * @param name The name of the project.
- * @param uid The user ID of the author.
- * @param file The code file(zip) to be uploaded.
- * @returns Project
- */
-export function saveProject(name: string, uid: number, file: File): Promise<Project> {
-    const url = '/project/save';
-    const formData = new FormData();
-    formData.append('name', name);
-    formData.append('uid', uid.toString());
-    formData.append('file', file);
-
-    return service({
-        url: url,
-        method: 'post',
-        data: formData,
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        },
-    });
-}
+import qiniu from 'qiniu-js'
 
 /**
  * Format spx code
@@ -48,4 +24,48 @@ export function formatSpxCode(body: string): Promise<AxiosResponse<ResponseData<
             'Content-Type': 'multipart/form-data'
         },
     });
+}
+
+async function getUploadToken() {
+    const response = await service.get('/project/upload-token')
+    return response.data.data
+}
+
+export async function uploadFile(file: File) {
+    const token = await getUploadToken()
+    if (!token) {
+        throw new Error('Unable to get upload token')
+    }
+    return new Promise((resolve, reject) => {
+
+        const observable = qiniu.upload(file, null, token)
+        observable.subscribe({
+            next(res) {
+                console.log('upload progress: ', res.total.percent)
+            },
+            error(err) {
+                console.error('Upload failed:', err)
+                reject(err)
+            },
+            complete(res) {
+                const url = import.meta.env.VITE_KODO_ADDRESS + '/' + `${res.key}`
+                resolve(url)
+            }
+        })
+    })
+}
+
+export async function getProjectList(uid: string): Promise<ProjectInfo[]> {
+    const response = await service.get(`/project/list/${uid}`)
+    return response.data.data
+}
+
+export async function loadProject(id: string): Promise<ProjectDetail> {
+    const response = await service.get(`/project/detail/${id}`)
+    return response.data.data
+}
+
+export async function saveProject(params: SaveProjectParams): Promise<ProjectDetail> {
+    const response = await service.post('/project/save', params)
+    return response.data.data
 }
