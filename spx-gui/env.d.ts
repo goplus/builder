978529947b2e96d109b5ce80d3d08a856a10452b
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-02-23 13:44:58 +0800

    use env as base url

--- spx-gui/env.d.ts
+++ spx-gui/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE: string
+  readonly VITE_API_BASE_URL: string
   readonly VITE_CASDOOR_ENDPOINT: string
   readonly VITE_CASDOOR_CLIENT_ID: string
   readonly VITE_CASDOOR_ORGANIZATION_NAME: string
