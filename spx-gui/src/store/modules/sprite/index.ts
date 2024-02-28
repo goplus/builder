4af9c77e536b40fa0ea060b285196e3aec6e3c76
姚昕玥 <1482089698@qq.com>
2024-02-28 12:50:32 +0800

    feat: rename scratch assets

--- spx-gui/src/store/modules/sprite/index.ts
+++ spx-gui/src/store/modules/sprite/index.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: Zhang Zhi Yang
  * @Date: 2024-02-07 21:43:44
- * @LastEditors: Zhang Zhi Yang
- * @LastEditTime: 2024-02-28 16:52:14
+ * @LastEditors: xuning 453594138@qq.com
+ * @LastEditTime: 2024-02-28 17:33:55
  * @FilePath: \spx-gui\src\store\modules\sprite\index.ts
  * @Description:
  */
@@ -47,11 +47,11 @@ export const useSpriteStore = defineStore('sprite', () => {
     }
   }
 
-  return {
-    list,
-    current,
-    addItem,
-    setCurrentByName,
-    removeItemByName
-  }
+    return {
+        list, current,
+        addItem,
+        setCurrentByName,
+        removeItemByName,
+        existsByName
+    }
 })
