01436d7440ab23fb7c06700befc0ee1e40965ef8
qing-wq <2702461713@qq.com>
2024-03-01 17:58:20 +0800

    feat: rebuild project runner

--- spx-gui/src/components/project-runner/ProjectRunner.vue
+++ spx-gui/src/components/project-runner/ProjectRunner.vue
@@ -2,18 +2,12 @@
  * @Author: Hu JingJing
  * @Date: 2024-02-22 17:57:05
  * @LastEditors: Hu JingJing
- * @LastEditTime: 2024-02-26 15:11:06
+ * @LastEditTime: 2024-03-01 17:38:55
  * @Description: 
 -->
 <template>
   <div class="project-runner">
-    <iframe
-      ref="iframe"
-      src="/ispx/runner.html"
-      class="runner"
-      frameborder="0"
-      @load="handleIframeLoad"
-    />
+    <iframe ref="iframe" class="runner" frameborder="0" @load="handleIframeLoad" />
   </div>
 </template>
 
@@ -27,14 +21,17 @@ interface CustomWindow extends Window {
 
 const props = defineProps<{ project: Project }>()
 
-const zipResp = fetch('/test.zip')
-
 const iframe = ref<HTMLIFrameElement | null>(null)
+
+const zipResp = props.project.zip
+
 const handleIframeLoad = async () => {
   const runnerWindow = iframe.value?.contentWindow as CustomWindow
+  runnerWindow.document.write(html)
   console.log(runnerWindow)
 
   const buf = await (await zipResp).arrayBuffer()
+  console.log(typeof runnerWindow.startWithZipBuffer)
   await runnerWindow.startWithZipBuffer(buf)
 }
 
@@ -48,6 +45,42 @@ const stop = async () => {
     iframe.value.src = ''
   }
 }
+
+const html = `
+<html>
+<head>
+    <meta charset="utf-8" />
+    <script src="/wasm_exec.js"><\/script>
+<script>
+"use strict";
+
+console.log('enter iframe')
+let goRunPromiseResolve;
+const goRunPromise = new Promise(resolve => {
+  goRunPromiseResolve = resolve;
+});
+
+window.startWithZipBuffer = async (buffer) => {
+  await goRunPromise;
+  // For maybe a bug in Go WASM, we need to recreate an Uint8Array
+  // in spite of the original type of \`buffer\` so that
+  // the function can be called from out of iframe.
+  const view = new Uint8Array(buffer);
+  // goLoadData is injected in the Go code
+  goLoadData(view);
+}
+
+(async () => {
+  const go = new Go();
+  const {instance} = await WebAssembly.instantiateStreaming(fetch("/main.wasm"), go.importObject)
+  go.run(instance);
+  goRunPromiseResolve();
+})()
+console.log('leave iframe')
+<\/script>
+<\/head>
+<\/html>
+`;
 
 defineExpose({
   run,
