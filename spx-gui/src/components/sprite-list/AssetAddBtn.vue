03be4f4ec8ed1c15522477fdb31fd7cfc49729f1
luoliwoshang <2643523683@qq.com>
2024-03-01 11:58:21 +0800

    chore:accord pr comment to refine comment of sprite name rule

--- spx-gui/src/components/sprite-list/AssetAddBtn.vue
+++ spx-gui/src/components/sprite-list/AssetAddBtn.vue
@@ -192,7 +192,8 @@ const spriteNameAllow = computed(() => {
 // ----------methods-----------------------------------------
 // check if the name is legal.
 const isAllowName = (name: string) => {
-  // name rules of spx
+  // spx code is go+ code, and the sprite name will compiled to an identifier of go+
+  // so sprite name rules is depend on the identifier rules of go+.
   let regex = /^[\u4e00-\u9fa5a-zA-Z_][\u4e00-\u9fa5a-zA-Z0-9_]*$/
   return regex.test(name) && !typeKeywords.includes(name) && !keywords.includes(name)
 }
