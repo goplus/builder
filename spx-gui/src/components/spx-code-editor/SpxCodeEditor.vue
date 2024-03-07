167edae6760db31e2dae2984c6b1732cea6a5d6f
luoliwoshang <2643523683@qq.com>
2024-03-07 08:54:15 +0800

    style:format style

--- spx-gui/src/components/spx-code-editor/SpxCodeEditor.vue
+++ spx-gui/src/components/spx-code-editor/SpxCodeEditor.vue
@@ -1,129 +1,115 @@
 <!--
  * @Author: Zhang Zhi Yang
  * @Date: 2024-01-15 15:30:26
- * @LastEditors: xuning 453594138@qq.com
- * @LastEditTime: 2024-02-06 20:50:40
- * @FilePath: /spx-gui/src/components/spx-code-editor/SpxCodeEditor.vue
+ * @LastEditors: Zhang Zhi Yang
+ * @LastEditTime: 2024-03-07 08:50:28
+ * @FilePath: \spx-gui\src\components\spx-code-editor\SpxCodeEditor.vue
  * @Description: 
 -->
 <template>
-    <div class="code-editor-space">
-        <div class="code-button">{{ $t('component.code') }}</div>
-        <n-button-group class="formatBtnGroup" size="small">
-            <n-button class="" @click="clear">{{ $t("editor.clear") }}</n-button>
-            <n-button class="formatBtn" @click="format">{{ $t("editor.format") }}</n-button>
-        </n-button-group>
-        <CodeEditor ref="code_editor" :modelValue="currentCode" @update:modelValue="onCodeChange" />
-    </div>
+  <div class="code-editor-space">
+    <div class="code-button">{{ $t('component.code') }}</div>
+    <n-button-group class="formatBtnGroup" size="small">
+      <n-button class="" @click="clear">{{ $t('editor.clear') }}</n-button>
+      <n-button class="formatBtn" @click="format">{{ $t('editor.format') }}</n-button>
+    </n-button-group>
+    <CodeEditor ref="code_editor" :model-value="currentCode" @update:model-value="onCodeChange" />
+  </div>
 </template>
-  
-<script setup lang="ts">
-import CodeEditor, { monaco } from "@/components/code-editor"
-import { onBeforeUnmount, onMounted, ref, watch, computed } from 'vue';
-import { useEditorStore } from "@/store"
-import { useSpriteStore } from '@/store/modules/sprite';
-import { storeToRefs } from 'pinia'
-import { NButton } from "naive-ui"
-const spriteStore = useSpriteStore()
-const store = useEditorStore();
-const code_editor = ref();
 
+<script setup lang="ts">
+import CodeEditor, { monaco } from '@/components/code-editor'
+import { ref, computed } from 'vue'
+import { useEditorStore } from '@/store'
+import { useSpriteStore } from '@/store/modules/sprite'
+import { NButton } from 'naive-ui'
+const spriteStore = useSpriteStore()
+const store = useEditorStore()
+const code_editor = ref()
 
 // watch the current sprite and set it's code to editor
 const currentCode = computed(() => {
-    return spriteStore.current?.code || ''
+  return spriteStore.current?.code || ''
 })
 
 // Listen for editor value change, sync to sprite store
 const onCodeChange = (value: string) => {
-    if (spriteStore.current) {
-        spriteStore.current.code = value
-    }
+  if (spriteStore.current) {
+    spriteStore.current.code = value
+  }
 }
 
 const clear = () => {
-    code_editor.value.clear()
+  code_editor.value.clear()
 }
 
 const format = () => {
-    code_editor.value.format()
+  code_editor.value.format()
 }
 
 // Listen for insert events triggered by store, registered with store.$onAction
 const triggerInsertSnippet = (snippet: monaco.languages.CompletionItem) => {
-    code_editor.value.insertSnippet(
-        snippet
-    )
+  code_editor.value.insertSnippet(snippet)
 }
 
-
 // register insertSnippet
-store.$onAction(({
-    name,
-    store,
-    args,
-    after,
-    onError,
-}) => {
-    after(() => {
-        if (name === "insertSnippet") {
-            const snippet = args[0] as monaco.languages.CompletionItem
-            triggerInsertSnippet(snippet)
-        }
-    })
+store.$onAction(({ name, store, args, after, onError }) => {
+  after(() => {
+    if (name === 'insertSnippet') {
+      const snippet = args[0] as monaco.languages.CompletionItem
+      triggerInsertSnippet(snippet)
+    }
+  })
 })
-
-
 </script>
 
 <style scoped>
 #code-editor {
-    height: 100%;
+  height: 100%;
 }
 
-
 .code-button {
-    background: #cdf5ef;
-    width: 80px;
-    height: auto;
-    text-align: center;
-    position: absolute;
-    top: -2px;
-    font-size: 18px;
-    border: 2px solid #00142970;
-    border-radius: 0 0 10px 10px;
-    z-index: 2;
+  background: #cdf5ef;
+  width: 80px;
+  height: auto;
+  text-align: center;
+  position: absolute;
+  top: -2px;
+  font-size: 18px;
+  border: 2px solid #00142970;
+  border-radius: 0 0 10px 10px;
+  z-index: 2;
 }
 
 .code-editor-space {
-    display: flex;
-    flex-direction: column;
-    height: calc(100% - 44px);
-    position: relative;
-    background: white;
-    padding: 4px;
+  display: flex;
+  flex-direction: column;
+  height: calc(100% - 44px);
+  position: relative;
+  background: white;
+  padding: 4px;
 }
 
 .formatBtnGroup {
-    z-index: 99;
-    right: 20px;
-    position: absolute;
+  z-index: 99;
+  right: 20px;
+  position: absolute;
 
-    .n-button {
-        background: #00000000;
-        color: #001429;
-        border: 1px solid black;
-    }
+  .n-button {
+    background: #00000000;
+    color: #001429;
+    border: 1px solid black;
+  }
 
-    .n-button:hover {
-        background: #ed729e20;
-    }
+  .n-button:hover {
+    background: #ed729e20;
+  }
 }
 
 .action,
 .sprite {
-    display: flex;
-    justify-content: end;
-    margin-top: 10px;
+  display: flex;
+  justify-content: end;
+  margin-top: 10px;
 }
 </style>
--- spx-gui/src/store/modules/editor/index.ts
+++ spx-gui/src/store/modules/editor/index.ts
@@ -2,27 +2,27 @@
  * @Author: Zhang Zhi Yang
  * @Date: 2024-01-15 15:53:46
  * @LastEditors: Zhang Zhi Yang
- * @LastEditTime: 2024-02-02 11:05:30
- * @FilePath: /builder/spx-gui/src/store/modules/editor/index.ts
- * @Description: 
+ * @LastEditTime: 2024-03-07 08:53:29
+ * @FilePath: \spx-gui\src\store\modules\editor\index.ts
+ * @Description:
  */
-import { defineStore } from "pinia";
-import { monaco } from "@/components/code-editor";
+import { defineStore } from 'pinia'
+import { monaco } from '@/components/code-editor'
 
 export const useEditorStore = defineStore('editor', () => {
-    /**
-     * @description: trigger the insertion function
-     * The code editor component is subscribed the event
-     * @param {monaco.languages.CompletionItem} snippet
-     * @return {*}
-     * @author Zhang zhi yang
-     */
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const insertSnippet = (snippet: monaco.languages.CompletionItem) => {
-        // This function is notify code-editor component's insert Function
-    }
+  /**
+   * @description: trigger the insertion function
+   * The code editor component is subscribed the event
+   * @param {monaco.languages.CompletionItem} snippet
+   * @return {*}
+   * @author Zhang zhi yang
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const insertSnippet = (snippet: monaco.languages.CompletionItem) => {
+    // This function is notify code-editor component's insert Function
+  }
 
-    return {
-        insertSnippet,
-    }
+  return {
+    insertSnippet
+  }
 })
