f3e2269ae39355e2b18afc8792e7dfefe4b1387c
wangyin <67945139+Mrkuib@users.noreply.github.com>
2024-03-08 18:44:11 +0800

    spx backend (#132)
    * feat: add project is_public
    
    * feat: add query public project
    
    * feat: add query private project and update is_public
    
    * feat: add upload asset
    
    * fix : update asset address
    
    * feat:implement incremental saving of files to the server disk
    
    * fix : sql env
    
    * deploy: add dockerfile
    
    * fix: copy src file into docker image
    
    * Update Dockerfile
    
    * feat : fill back
    
    * feat : fill back
    
    * feat: asset list support category
    
    * feat: asset list support category
    
    * feat : search asset
    
    * feat : search asset
    
    * feat : search asset
    
    * feat : search asset
    
    * feat : search asset
    
    * feat: asset save
    
    * feat : search asset
    
    * feat: asset save
    
    * feat : env
    
    * feat:refactor file loading and saving
    
    * feat: add click count, asset list order by time/hot
    
    * fix:fix variable case
    
    * feat : spirits to gif interface
    
    * fix :search bug
    
    * fix :search bug
    
    * fix :search bug
    
    * fix : sql format
    
    * feat : update asset list
    
    * feat : insert token
    
    * feat : middleware
    
    * fix : format
    
    * fix : project list
    
    * fix : project list
    
    * fix : delete asset upload
    
    * fix : delete asset upload
    
    * fix : delete user func
    
    * fix : delete user func
    
    * fix : delete project
    
    * feat : insert exegesis
    
    * feat : insert exegesis
    
    * fix : Split interfaces
    
    * fix : Format .gox
    
    * fix : Format .gox
    
    * fix : Format .gox
    
    * fix : Format .gox
    
    * fix : fix code
    
    * fix : fix code
    
    * fix : fix AddProject
    
    * fix : Support a single image to become a sprite
    
    * fix: add comment
    
    * fix : code
    
    * fix : code
    
    * fix : code
    
    * fix: click count
    
    * fix : code
    
    * fix : code
    
    * fix : code
    
    * fix : code
    
    * fix : code
    
    * fix : delete logic
    
    * fix : code
    
    * fix : code
    
    * fix : code currentUid
    
    * fix : env
    
    ---------
    
    Co-authored-by: wxy <1939311091@qq.com>
    Co-authored-by: TerryW-tx <517557008@qq.com>
    Co-authored-by: 姚昕玥 <1482089698@qq.com>

---
+++ spx-backend/internal/common/constants.go
@@ -0,0 +1,57 @@
+package common
+
+const (
+	PERSONAL = 0
+	PUBLIC   = 1
+
+	SPRITE     = "0"
+	BACKGROUND = "1"
+	SOUND      = "2"
+)
+const (
+	SUCCESS                 = 200
+	NoLogin                 = 401
+	ERROR                   = 500
+	ErrorProjectNotFound    = 10001
+	ErrorDeleteProject      = 10002
+	ErrorUpdateProjectState = 10003
+	ErrorGetAsset           = 10004
+	ErrorSave               = 10005
+	ErrorGetProjects        = 10006
+	ErrClick                = 10007
+
+	ErrorPermissions        = 20001
+	ErrorParseMultipartForm = 20002
+	ErrorImagesToGif        = 20003
+	ErrorUpload             = 20004
+	ErrorNameNotNull        = 20005
+)
+
+var MsgFlags = map[int]string{
+	SUCCESS: "ok",
+	ERROR:   "Fail",
+	NoLogin: "Unauthorized",
+
+	ErrorProjectNotFound:    "Project not found",
+	ErrorDeleteProject:      "Delete error",
+	ErrorUpdateProjectState: "Update project is_public error",
+	ErrorGetAsset:           "Get asset error",
+	ErrorGetProjects:        "Get projects error",
+	ErrClick:                "Click err",
+
+	ErrorPermissions:        "No Permissions",
+	ErrorParseMultipartForm: "Parse Multipart Form Error",
+	ErrorImagesToGif:        "Generate gif error",
+	ErrorUpload:             "Upload err",
+	ErrorNameNotNull:        "please fill name",
+	ErrorSave:               "save err",
+}
+
+// GetMsg 获取状态码对应信息
+func GetMsg(code int) string {
+	msg, ok := MsgFlags[code]
+	if ok {
+		return msg
+	}
+	return MsgFlags[ERROR]
+}
