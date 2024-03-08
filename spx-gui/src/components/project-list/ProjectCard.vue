e4eadce8b02a41350d8efeb91e9b2dfbf9ca6009
TuGitee <tgb@std.uestc.edu.cn>
2024-03-08 16:49:03 +0800

    chore: change class name

--- spx-gui/src/components/project-list/ProjectCard.vue
+++ spx-gui/src/components/project-list/ProjectCard.vue
@@ -39,7 +39,7 @@
         </n-tag>
       </p>
       <p v-if="!isLocal" :style="statusStyle" class="public-status">status: {{ publicStatusText(publicStatus) }}</p>
-      <p class="create-time">version: {{ project.version }} </p>
+      <p class="version">version: {{ project.version }} </p>
       <p class="create-time">create: {{ formatTime(project.cTime) }} </p>
       <p class="update-time">update: {{ formatTime(project.uTime) }} </p>
     </div>
