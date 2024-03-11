d6e601fdf9ffb02594e233521f31d69a49eddf37
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-03-10 21:11:49 -0700

    add pr-preview & merge pr preview container with deployment (#162)
    * add pr-preview
    
    * chmod +x
    
    * add all-in-one dockerfile
    
    * update config for backend
    
    * update workflow
    
    * add comment
    
    trigger ci
    
    debug with log
    
    debug
    
    debug
    
    fix
    
    fix
    
    * clean up

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+# All-in-one Dockerfile for building the SPX GUI
+# TODO: build backend.
+
+FROM golang:1.21 as go-builder
+
+WORKDIR /app
+
+# Build WASM
+COPY tools ./tools
+COPY spx-backend ./spx-backend
+
+WORKDIR /app/tools/fmt
+RUN ./build.sh
+
+WORKDIR /app/tools/ispx
+RUN ./build.sh
+
+# Install Go+
+RUN bash -c ' echo "deb [trusted=yes] https://pkgs.goplus.org/apt/ /" > /etc/apt/sources.list.d/goplus.list' \
+    && apt update \
+    && apt install -y gop
+
+# Build backend
+WORKDIR /app/spx-backend/cmd
+RUN gop build -o spx-backend .
+
+FROM node:latest as frontend-builder
+
+WORKDIR /app/spx-gui
+
+COPY spx-gui/package.json spx-gui/package-lock.json ./
+
+RUN npm install
+
+COPY spx-gui .
+# Required to resolve symlinks
+COPY tools ../tools
+COPY --from=go-builder /app/tools/fmt/static/main.wasm /app/spx-gui/src/assets/format.wasm
+COPY --from=go-builder /app/tools/ispx/main.wasm /app/spx-gui/src/assets/ispx/main.wasm
+
+RUN npm run build
+
+FROM nginx:bookworm
+
+COPY --from=frontend-builder /app/spx-gui/dist /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+# Compress WASM files for gzip_static
+RUN find /usr/share/nginx/html -name "*.wasm" -exec gzip -9 -k {} \;
+
+EXPOSE 80
+
+COPY --from=go-builder /app/spx-backend/cmd/spx-backend /usr/local/bin/spx-backend
+
+CMD spx-backend & nginx -g "daemon off;" & wait
---
+++ scripts/pr-preview.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+###########################################
+# This script is used to start the application in the PR environment
+###########################################
+set -ex
+
+echo "WORKSPACE: ${PWD}"
+
+# TODO: Include the logic to also build the backend once the PR is merged
+
+export CONTAINER_IMAGE=aslan-spock-register.qiniu.io/goplus/goplus-builder-pr:${PULL_NUMBER}-${PULL_PULL_SHA:0:8}
+docker build -t ${CONTAINER_IMAGE} -f ./Dockerfile . --builder="kube" --push
+
+# generate kubernetes yaml with unique flag for PR
+cat > builder.yaml << EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: goplus-builder-pr-${PULL_NUMBER}
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: goplus-builder-pr-${PULL_NUMBER}
+  template:
+    metadata:
+      labels:
+        app: goplus-builder-pr-${PULL_NUMBER}
+    spec:
+      containers:
+        - name: my-container
+          image: ${CONTAINER_IMAGE}
+          ports:
+            - containerPort: 80
+---            
+apiVersion: v1
+kind: Service
+metadata:
+  name: goplus-builder-pr-${PULL_NUMBER}
+spec:
+  selector:
+    app: goplus-builder-pr-${PULL_NUMBER}
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 80
+  type: ClusterIP
+EOF
+
+kubectl -n goplus-pr-review apply -f builder.yaml 
+
+kubectl -n goplus-pr-review get pods
+
+# comment on the PR
+PREVIEW_URL=http://goplus-builder-pr-${PULL_NUMBER}.goplus-pr-review.svc.jfcs-qa1.local
+message=$'The PR environment is ready, please check the [PR environment]('${PREVIEW_URL}')
+
+[Attention]: This environment will be automatically cleaned up after 6 hours, please make sure to test it in time. If you have any questions, please contact the author of the PR or the SPX Builder team. 
+'
+gh_comment -org=${REPO_OWNER} -repo=${REPO_NAME} -num=${PULL_NUMBER} -p=${PREVIEW_URL} -b "${message}"
+
