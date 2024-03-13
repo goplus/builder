9084a98d0a3d41cafbc678c676a4b75f4863adc7
xuning <453594138@qq.com>
2024-03-13 13:48:59 +0800

    fix: delete comment header

--- spx-gui/src/components/top-menu/UserAvatar.vue
+++ spx-gui/src/components/top-menu/UserAvatar.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: xuning 453594138@qq.com
- * @Date: 2024-03-12 15:42:02
- * @LastEditors: xuning 453594138@qq.com
- * @LastEditTime: 2024-03-12 16:34:21
- * @FilePath: /builder/spx-gui/src/components/top-menu/UserAvatar.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div>
     <n-button v-if="!userStore.userInfo" :disabled="networkStore.offline()" @click="signin()">{{ $t('tab.signIn') }}</n-button>
