2055ea17835ac39a5368aeb60b707e352c5a6ef5
Changjun Ji <jinsdu@outlook.com>
2024-04-02 11:25:26 +0800

    ci: adjust pr preview environment's recycling time (#277)

--- scripts/pr-preview.sh
+++ scripts/pr-preview.sh
@@ -11,6 +11,7 @@ PREVIEW_URL=http://goplus-builder-pr-${PULL_NUMBER}.goplus-pr-review.svc.jfcs-qa
 echo VITE_PUBLISH_BASE_URL=${PREVIEW_URL}/ > spx-gui/.env.local
 export CONTAINER_IMAGE=aslan-spock-register.qiniu.io/goplus/goplus-builder-pr:${PULL_NUMBER}-${PULL_PULL_SHA:0:8}
 docker build -t ${CONTAINER_IMAGE} -f ./Dockerfile . --builder="kube" --push
+export CURRENT_TIME=$(date "--iso-8601=seconds")
 
 # generate kubernetes yaml with unique flag for PR
 cat > builder.yaml << EOF
@@ -18,6 +19,10 @@ apiVersion: apps/v1
 kind: Deployment
 metadata:
   name: goplus-builder-pr-${PULL_NUMBER}
+  labels:
+    sleepmode.kubefree.com/delete-after: "144h"
+  annotations:
+    sleepmode.kubefree.com/activity-status: '{"LastActivityTime": "${CURRENT_TIME}"}'
 spec:
   replicas: 1
   selector:
@@ -38,6 +43,10 @@ apiVersion: v1
 kind: Service
 metadata:
   name: goplus-builder-pr-${PULL_NUMBER}
+  labels:
+    sleepmode.kubefree.com/delete-after: "144h"
+  annotations:
+    sleepmode.kubefree.com/activity-status: '{"LastActivityTime": "${CURRENT_TIME}"}'
 spec:
   selector:
     app: goplus-builder-pr-${PULL_NUMBER}
@@ -55,7 +64,7 @@ kubectl -n goplus-pr-review get pods
 # comment on the PR
 message=$'The PR environment is ready, please check the [PR environment]('${PREVIEW_URL}')
 
-[Attention]: This environment will be automatically cleaned up after 6 hours, please make sure to test it in time. If you have any questions, please contact the author of the PR or the SPX Builder team. 
+[Attention]: This environment will be automatically cleaned up after a certain period of time., please make sure to test it in time. If you have any questions, please contact the builder team.
 '
 gh_comment -org=${REPO_OWNER} -repo=${REPO_NAME} -num=${PULL_NUMBER} -p=${PREVIEW_URL} -b "${message}"
 
