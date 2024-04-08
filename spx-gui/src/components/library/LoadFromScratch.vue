7868b84f1e25f145aa22e2240dfae98bee9d7b4f
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-04-08 18:14:10 +0800

    refactor: import from scratch

--- spx-gui/src/components/library/LoadFromScratch.vue
+++ spx-gui/src/components/library/LoadFromScratch.vue
@@ -1,271 +1,138 @@
 <template>
-  <n-spin :show="isAssetInfosLoading">
-    <div class="file-upload-container">
-      <button type="button" class="custom-upload-btn" @click="triggerFileUpload">
-        {{ $t('scratch.upload') }}
-      </button>
-      <input
-        ref="fileUploadInput"
-        type="file"
-        accept=".sb3"
-        style="display: none"
-        @change="handleScratchFileUpload"
-      />
-      <button
-        v-if="selectedAssets.length != 0"
-        class="custom-import-btn"
-        @click="importSelectedAssetsToProject"
-      >
-        {{ $t('scratch.importToSpx') }}
-      </button>
-      <button
-        v-if="selectedAssets.length != 0"
-        class="custom-import-btn"
-        @click="uploadSelectedAssetsToPersonalLibrary"
-      >
-        {{ $t('scratch.uploadToPrivateLibrary') }}
-      </button>
-    </div>
-    <div class="asset-detail-info">
-      <n-grid cols="3 s:4 m:5 l:6 xl:7 2xl:8" responsive="screen">
-        <n-grid-item
-          v-for="assetFileDetail in assetFileDetails"
-          :key="assetFileDetail.url"
-          class="file-row"
-          :style="{
-            border: selectedAssets.includes(assetFileDetail)
-              ? `3px solid ${commonColor}`
-              : '3px solid #eeeeee'
-          }"
-          @click="chooseAssets(assetFileDetail)"
-        >
-          <n-input
-            v-model:value="assetFileDetail.name"
-            placeholder="assetFileDetail.name"
-            size="tiny"
-            style="width: 80%; margin-top: 2px"
-            @click.stop="() => {}"
-          >
-            <template #suffix>
-              .
-              <span style="color: #aa9b9b">
-                {{ assetFileDetail.extension }}
-              </span>
-            </template>
-          </n-input>
-          <n-image
-            v-if="isImage(assetFileDetail)"
-            style="position: absolute; top: 30px; margin: auto; border-radius: 20px"
-            preview-disabled
-            width="80"
-            height="80"
-            :src="assetFileDetail.url"
-            :fallback-src="error"
-          />
-          <n-image
-            v-else
-            preview-disabled
-            style="position: absolute; top: 30px; margin: auto; border-radius: 20px"
-            width="80"
-            height="80"
-            :src="SoundsImport"
-            :fallback-src="error"
-            @click.stop="playAudio(assetFileDetail.url)"
-          />
-          <div class="file-btn">
-            <n-button
-              type="primary"
-              secondary
-              style="height: 24px; padding: 6px"
-              @click.stop="downloadAsset(assetFileDetail)"
-            >
-              {{ $t('scratch.download') }}
-            </n-button>
-          </div>
-        </n-grid-item>
-      </n-grid>
-    </div>
-  </n-spin>
+  <div class="file-upload-container">
+    <button type="button" class="custom-upload-btn" @click="triggerFileUpload">
+      {{ $t('scratch.upload') }}
+    </button>
+    <input
+      ref="fileUploadInput"
+      type="file"
+      accept=".sb3"
+      style="display: none"
+      @change="handleScratchFileUpload"
+    />
+  </div>
+  <div class="asset-detail-info">
+    <div>Sprites TODO: i18n / re-style / refactor as components</div>
+    <n-grid cols="3 s:4 m:5 l:6 xl:7 2xl:8" responsive="screen">
+      <n-grid-item v-for="asset in sprites" :key="asset.name" class="file-row">
+        <div>{{ asset.name }}.{{ asset.extension }}</div>
+        <n-image
+          style="position: absolute; top: 30px; margin: auto; border-radius: 20px"
+          preview-disabled
+          width="80"
+          height="80"
+          :src="asset.src"
+          :fallback-src="error"
+        />
+        <NButton @click="importAssetToProject(asset)">Import </NButton>
+      </n-grid-item>
+    </n-grid>
+    <div>Sounds</div>
+    <n-grid cols="3 s:4 m:5 l:6 xl:7 2xl:8" responsive="screen">
+      <n-grid-item v-for="asset in sounds" :key="asset.name" class="file-row">
+        <div>{{ asset.name }}.{{ asset.extension }}</div>
+        <n-image
+          preview-disabled
+          style="position: absolute; top: 30px; margin: auto; border-radius: 20px"
+          width="80"
+          height="80"
+          :src="soundsImportSvg"
+          :fallback-src="error"
+          @click="playAudio(asset)"
+        />
+        <NButton @click="importAssetToProject(asset)">Import </NButton>
+      </n-grid-item>
+    </n-grid>
+    <div>Backdrops</div>
+    <n-grid cols="3 s:4 m:5 l:6 xl:7 2xl:8" responsive="screen">
+      <n-grid-item v-for="asset in backdrops" :key="asset.name" class="file-row">
+        <div>{{ asset.name }}.{{ asset.extension }}</div>
+        <n-image
+          style="position: absolute; top: 30px; margin: auto; border-radius: 20px"
+          preview-disabled
+          width="80"
+          height="80"
+          :src="asset.src"
+          :fallback-src="error"
+        />
+        <NButton @click="importAssetToProject(asset)">Import </NButton>
+      </n-grid-item>
+    </n-grid>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import { Sound } from '@/models/sound'
-import {
-  type MessageApi,
-  NButton,
-  NGrid,
-  NGridItem,
-  NImage,
-  NInput,
-  useMessage,
-  NSpin
-} from 'naive-ui'
+import { NButton, NGrid, NGridItem, NImage, useMessage } from 'naive-ui'
 import { Sprite } from '@/models/sprite'
-import { useEditorCtx } from '@/components/editor/ProjectEditor.vue'
-import { commonColor } from '@/assets/theme'
 import error from '@/assets/error.svg'
-import { type AssetFileDetail as ScratchAssetFile, parseScratchFile } from '@/utils/scratch'
-import saveAs from 'file-saver'
-import { addAsset, IsPublic } from '@/apis/asset'
-import { useI18n } from 'vue-i18n'
+import { parseScratchFileAssets, type ExportedScratchAsset } from '@/utils/scratch'
+import soundsImportSvg from './images/sound-import.svg'
+import { Backdrop } from '@/models/backdrop'
 import { Costume } from '@/models/costume'
 import { fromBlob } from '@/models/common/file'
-import { sound2Asset, sprite2Asset } from '@/models/common'
-import SoundsImport from './images/sound-import.svg'
+import { computed } from 'vue'
+import { useEditorCtx } from '../editor/ProjectEditor.vue'
 
 const editorCtx = useEditorCtx()
-const message: MessageApi = useMessage()
 
-// ----------data related -----------------------------------
 // Ref about asset infos.
-const assetFileDetails = ref<ScratchAssetFile[]>([])
-// Ref about an array of selected assets infos.
-const selectedAssets = ref<ScratchAssetFile[]>([])
-// Ref about file upload input component.
-const fileUploadInput = ref(null)
-// loading state
-const isAssetInfosLoading = ref<boolean>(false)
-const { t } = useI18n({
-  inheritLocale: true
-})
+const assetFileDetails = ref<ExportedScratchAsset[]>([])
+const sprites = computed(() => assetFileDetails.value.filter((asset) => asset.type === 'sprite'))
+const sounds = computed(() => assetFileDetails.value.filter((asset) => asset.type === 'sound'))
+const backdrops = computed(() =>
+  assetFileDetails.value.filter((asset) => asset.type === 'backdrop')
+)
+const fileUploadInput = ref<HTMLInputElement>()
 
-// ----------methods-----------------------------------------
-/**
- * @description: Return true if asset is an image
- * @param {*} assetDetail
- * @return {*}
- */
-const isImage = (assetDetail: ScratchAssetFile) => {
-  return ['svg', 'jpeg', 'jpg', 'png'].includes(assetDetail.extension.toLowerCase())
-}
+const message = useMessage()
 
-/**
- * @description: Trigger File Upload UI.
- * @return {*}
- */
 function triggerFileUpload() {
   if (fileUploadInput.value) {
     ;(fileUploadInput.value as HTMLInputElement).click()
   }
 }
 
-/**
- * @description: Handle Scratch File Upload. Only.sb3 files are supported
- * @param {*} event
- * @return {*}
- */
 const handleScratchFileUpload = async (event: Event) => {
   let input = event.target as HTMLInputElement
   if (!input.files || input.files.length === 0) {
     return
   }
-  assetFileDetails.value = await parseScratchFile(input.files[0])
+  assetFileDetails.value = await parseScratchFileAssets(input.files[0])
 }
 
-/**
- * @description:Add a method for playing audio directly in the browser
- * @param {*} audioUrl
- * @return {*}
- */
-const playAudio = (audioUrl: string) => {
-  let audio = new Audio(audioUrl)
+const playAudio = async (asset: ExportedScratchAsset) => {
+  let audio = new Audio(asset.src)
   audio.play()
 }
 
-/**
- * @description: Choose assets
- * @param {*} asset
- * @return {*}
- */
-const chooseAssets = (asset: ScratchAssetFile) => {
-  if (selectedAssets.value) {
-    let index = selectedAssets.value.findIndex((a) => a.name === asset.name)
-    if (index !== -1) {
-      selectedAssets.value.splice(index, 1)
-    } else {
-      selectedAssets.value.push(asset)
+async function importAssetToProject({ blob, name, extension, type }: ExportedScratchAsset) {
+  const filename = `${name}.${extension}`
+  switch (type) {
+    case 'sprite': {
+      const file = fromBlob(filename, blob)
+      const costume = new Costume(name, file, {})
+      const sprite = new Sprite(name, '', [costume], {})
+      editorCtx.project.addSprite(sprite)
+      break
     }
+    case 'sound': {
+      const sound = new Sound(name, fromBlob(filename, blob), {})
+      editorCtx.project.addSound(sound)
+      break
+    }
+    case 'backdrop': {
+      const file = fromBlob(filename, blob)
+      const backdrop = new Backdrop(name, file, {})
+      editorCtx.project.stage.addBackdrop(backdrop) // TODO: replace instead of add backdrop
+      break
+    }
+    default:
+      message.error(`Unsupported asset type: ${type}`)
+      throw new Error(`Unsupported asset type: ${type}`)
   }
-}
-
-/**
- * @description: Download one asset file
- * @param {*} asset
- * @return {*}
- */
-const downloadAsset = (asset: ScratchAssetFile) => {
-  saveAs(asset.blob, asset.name + '.' + asset.extension)
-}
-
-/**
- * @description: Import the selected assets to project.
- * @return {*}
- */
-const importSelectedAssetsToProject = async () => {
-  if (!selectedAssets.value) {
-    isAssetInfosLoading.value = false
-    return
-  }
-  await Promise.all(
-    selectedAssets.value.map(async (asset) => {
-      const spriteOrSound = await scratchAsset2Asset(asset)
-      if (spriteOrSound instanceof Sprite) {
-        editorCtx.project.addSprite(spriteOrSound)
-      } else {
-        editorCtx.project.addSound(spriteOrSound)
-      }
-    })
-  )
-  showImportSuccessMessage()
-}
-
-const uploadSelectedAssetsToPersonalLibrary = async () => {
-  isAssetInfosLoading.value = true
-  if (!selectedAssets.value) {
-    isAssetInfosLoading.value = false
-    return
-  }
-  await Promise.all(
-    selectedAssets.value.map(async (asset) => {
-      const spriteOrSound = await scratchAsset2Asset(asset)
-      const assetData = await (spriteOrSound instanceof Sprite
-        ? sprite2Asset(spriteOrSound)
-        : sound2Asset(spriteOrSound))
-      await addAsset({
-        ...assetData,
-        displayName: asset.name,
-        category: '',
-        isPublic: IsPublic.personal,
-        preview: 'TODO' // TODO
-      })
-    })
-  )
-  showImportSuccessMessage()
-}
-
-async function scratchAsset2Asset(scratchFile: ScratchAssetFile) {
-  if (isImage(scratchFile)) {
-    const costumeName = 'default'
-    const costumeFile = await fromBlob(costumeName + '.' + scratchFile.extension, scratchFile.blob)
-    const costume = new Costume(costumeName, costumeFile, {})
-    return new Sprite(scratchFile.name, '', [costume], {})
-  } else {
-    const soundFile = await fromBlob(
-      scratchFile.name + '.' + scratchFile.extension,
-      scratchFile.blob
-    )
-    return new Sound(scratchFile.name, soundFile, {})
-  }
-}
-
-/**
- * @description: Show 'import successfully' message
- * @return {*}
- */
-const showImportSuccessMessage = () => {
-  isAssetInfosLoading.value = false
-  message.success(t('message.import'), { duration: 1000 })
+  message.success(`Imported ${name}.${extension} TODO: use \`useMessageHandle\` for i18n`)
 }
 </script>
 
--- spx-gui/src/utils/scratch.ts
+++ spx-gui/src/utils/scratch.ts
@@ -1,75 +1,99 @@
-/*
- * @Author: Yao xinyue kother@qq.com
- * @Date: 2024-02-29 12:00:04
- * @LastEditors: xuning 453594138@qq.com
- * @LastEditTime: 2024-02-29 15:38:04
- * @FilePath: src/util/scratch.ts
- * @Description: The util of scratch
- */
-
 import JSZip from 'jszip'
-import { getMimeFromExt } from '@/utils/file'
+import { getMimeFromExt } from './file'
 
 /**
  * Represents the detailed information of an asset extracted from a Scratch project file.
  */
-export interface AssetFileDetail {
+export interface ExportedScratchAsset {
+  type: ExportedScratchAssetType
   name: string
   extension: string
-  url: string
   blob: Blob
+  src: string
+}
+
+export type ExportedScratchAssetType = 'sprite' | 'sound' | 'backdrop' | 'unknown'
+
+interface ScratchFile {
+  name: string
+  assetId: string
+  dataFormat: string
+  format: string
+  rate: number
+  sampleCount: number
+  md5ext: string
 }
 
 /**
  * Describes the structure of a Scratch project
  */
 interface ScratchProject {
-  targets: Array<{
-    costumes: Array<{ md5ext: string; name: string; dataFormat: string }>
-    sounds: Array<{ md5ext: string; name: string; dataFormat: string }>
-  }>
+  targets: {
+    isStage: boolean
+    costumes: ScratchFile[]
+    sounds: ScratchFile[]
+  }[]
 }
 
-/**
- * Parses a Scratch project file to extract and return asset details.
- *
- * @param {File} file - The Scratch project file to be parsed. (project.sb3)
- * @returns {Promise<AssetFileDetail[]>} A promise that resolves to an array of asset file details.
- */
-export const parseScratchFile = async (file: File): Promise<AssetFileDetail[]> => {
+export const parseScratchFileAssets = async (file: File): Promise<ExportedScratchAsset[]> => {
   const zip = await JSZip.loadAsync(file)
   const projectJson = await zip.file('project.json')?.async('string')
   if (!projectJson) throw new Error('Project JSON not found in the uploaded file.')
 
   const projectData: ScratchProject = JSON.parse(projectJson)
-  const assetNameMap = new Map<string, string>()
+  const assetNameMap = new Map<
+    string, // filename
+    {
+      name: string
+      dataFormat: string
+      isStage: boolean
+    }
+  >()
 
   projectData.targets.forEach((target) => {
-    target.costumes.forEach((costume) => {
-      assetNameMap.set(costume.md5ext, costume.name + '.' + costume.dataFormat)
-    })
-    target.sounds.forEach((sound) => {
-      assetNameMap.set(sound.md5ext, sound.name + '.' + sound.dataFormat)
-    })
+    const f = (asset: ScratchFile) => {
+      // A special case for the stage: md5ext is empty
+      const filename = asset.md5ext ? asset.md5ext : `${asset.assetId}.${asset.dataFormat}`
+      assetNameMap.set(filename, {
+        isStage: target.isStage,
+        name: asset.name,
+        dataFormat: asset.dataFormat
+      })
+    }
+    target.costumes.forEach(f)
+    target.sounds.forEach(f)
   })
 
-  const assetFileDetails: AssetFileDetail[] = []
+  const assetFileDetails: ExportedScratchAsset[] = []
 
-  for (const filename of Object.keys(zip.files)) {
-    const extensionMatch = filename.match(/\.(svg|jpeg|jpg|png|wav|mp3)$/)
-    if (extensionMatch) {
-      const originalName = assetNameMap.get(filename)
-      if (!originalName) continue
-      const zipFile = zip.file(filename)
-      if (!zipFile) continue
-      const fileData = await zipFile.async('blob')
-      const mimeType = getMimeFromExt(extensionMatch[1])
-      const blob = new Blob([fileData], { type: mimeType })
-      const url = URL.createObjectURL(blob)
-      const name = originalName.split('.').slice(0, -1).join('.')
-      const extension = originalName.split('.').pop() || ''
-      assetFileDetails.push({ name, extension, url, blob })
-    }
+  for (const [filename, zipFile] of Object.entries(zip.files)) {
+    const metadata = assetNameMap.get(filename)
+    if (!metadata) continue
+
+    const arrayBuffer = await zipFile.async('arraybuffer')
+    const blob = new Blob([arrayBuffer], { type: getMimeFromExt(metadata.dataFormat) })
+
+    assetFileDetails.push({
+      type: typeByExtension(metadata.dataFormat, metadata.isStage),
+      name: metadata.name,
+      extension: metadata.dataFormat,
+      blob,
+      src: URL.createObjectURL(blob)
+    })
   }
+
+  console.log(assetFileDetails)
   return assetFileDetails
+}
+
+const isAudio = (extension: string) => getMimeFromExt(extension).startsWith('audio')
+const isImage = (extension: string) => getMimeFromExt(extension).startsWith('image')
+
+const typeByExtension = (extension: string, isStage: boolean): ExportedScratchAssetType => {
+  if (isAudio(extension)) return 'sound'
+  if (isImage(extension)) {
+    if (isStage) return 'backdrop'
+    return 'sprite'
+  }
+  return 'unknown'
 }
