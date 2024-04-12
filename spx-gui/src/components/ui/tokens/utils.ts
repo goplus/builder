3ce64de52f3445db306ffb3fbe3297206b9a5bc6
nighca <nighca@live.cn>
2024-04-12 16:10:15 +0800

    update comment

--- spx-gui/src/components/ui/tokens/utils.ts
+++ spx-gui/src/components/ui/tokens/utils.ts
@@ -2,8 +2,8 @@ type VarValue = string | Vars
 type Vars = { [key: string]: VarValue }
 
 export function getCssVars(prefix: string, uiVars: Vars) {
-  return Object.entries(uiVars).reduce<{ [key: string]: string }>((cssVars, [key, value]) => {
-    // key should be guaranteed to be camel-case
+  return Object.entries(uiVars).reduce<{ [k: string]: string }>((cssVars, [key, value]) => {
+    // the original key (of `uiVars`) should be guaranteed to be camel-case
     key = prefix + key.replace(/([A-Z])/g, '-$1').toLowerCase()
     if (typeof value === 'string') {
       cssVars[key] = value
