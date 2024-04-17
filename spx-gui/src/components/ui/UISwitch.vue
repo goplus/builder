a32de10fe7b6451ecf4142c363cac7a20f31f439
Hanxing Yang <nighca@live.cn>
2024-04-17 11:28:39 +0800

    finish (almostly) input components (#323)

---
+++ spx-gui/src/components/ui/UISwitch.vue
@@ -0,0 +1,22 @@
+<template>
+  <NSwitch v-bind="props" @update:value="(v) => emit('update:value', v)">
+    <slot></slot>
+  </NSwitch>
+</template>
+
+<script setup lang="ts">
+import { NSwitch } from 'naive-ui'
+
+const props = defineProps<{
+  value?: boolean
+  disabled?: boolean
+}>()
+
+const emit = defineEmits<{
+  'update:value': [boolean]
+}>()
+</script>
+
+<style lang="scss" scoped>
+// TODO: switch style not designed yet
+</style>
---
+++ spx-gui/src/components/ui/radio/UIRadioGroup.vue
@@ -0,0 +1,20 @@
+<template>
+  <NRadioGroup v-bind="props" @update:value="(v) => emit('update:value', v)">
+    <slot></slot>
+  </NRadioGroup>
+</template>
+
+<script setup lang="ts">
+import { NRadioGroup } from 'naive-ui'
+
+const props = defineProps<{
+  value?: string | null
+  disabled?: boolean
+}>()
+
+const emit = defineEmits<{
+  'update:value': [string | null]
+}>()
+</script>
+
+<style lang="scss" scoped></style>
