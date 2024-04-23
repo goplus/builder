03b2ff38a6c7b86f0e5bb964627f6235725b2f58
nighca <nighca@live.cn>
2024-04-23 12:10:38 +0800

    UI message

--- spx-gui/src/components/ui/message/index.ts
+++ spx-gui/src/components/ui/message/index.ts
@@ -1,19 +1,26 @@
-import { useMessage as useNMessage } from 'naive-ui'
+import { useMessage as useNMessage, type MessageOptions } from 'naive-ui'
 
-// TODO: style design for message not finished yet
+export { default as UIMessageProvider } from './UIMessageProvider.vue'
 
 export function useMessage() {
   const nMessage = useNMessage()
 
+  const options: MessageOptions = {
+    closable: false
+  }
+
   return {
     info(content: string) {
-      nMessage.info(content)
+      nMessage.info(content, options)
     },
     success(content: string) {
-      nMessage.success(content)
+      nMessage.success(content, options)
+    },
+    warning(content: string) {
+      nMessage.warning(content, options)
     },
     error(content: string) {
-      nMessage.error(content)
+      nMessage.error(content, options)
     }
     // We do not provide loading message (which do loading-feedback globally),
     // instead, we do loading-feedback in-place, with
