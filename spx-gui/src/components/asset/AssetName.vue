5047f43e454f74cfb1a65f6f544bdf8e6e5460d6
nighca <nighca@live.cn>
2024-04-28 19:30:56 +0800

    limit display length of asset' name

---
+++ spx-gui/src/components/asset/AssetName.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="asset-name">
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped lang="scss">
+.asset-name {
+  max-width: 16em;
+  overflow-x: hidden;
+  word-break: keep-all;
+  text-overflow: ellipsis;
+}
+</style>
