9e61f2bed2f86e1449c48ad08646a7c7da51fe06
Aofei Sheng <aofei@aofeisheng.com>
2024-05-09 14:20:45 +0800

    spx-backend: refactor internal/utils/fmtcode
    Fixes #395

--- spx-backend/go.mod
+++ spx-backend/go.mod
@@ -9,14 +9,15 @@ require (
 	github.com/goplus/yap v0.7.2 //gop:class
 	github.com/joho/godotenv v1.5.1
 	github.com/qiniu/go-cdk-driver v0.1.0
-	github.com/qiniu/x v1.13.8
+	github.com/qiniu/x v1.13.10
 	gocloud.dev v0.36.0 // indirect
-	golang.org/x/mod v0.14.0
-	golang.org/x/tools v0.17.0
+	golang.org/x/mod v0.17.0
+	golang.org/x/tools v0.19.0
 )
 
 require (
 	github.com/casdoor/casdoor-go-sdk v0.36.0
+	github.com/goplus/gop v1.2.6
 	github.com/qiniu/go-sdk/v7 v7.18.0
 	github.com/stretchr/testify v1.8.1
 )
@@ -27,12 +28,13 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/goplus/gogen v1.15.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/oauth2 v0.14.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	google.golang.org/api v0.151.0 // indirect
--- spx-backend/internal/utils/fmtcode/fmtcode.go
+++ spx-backend/internal/utils/fmtcode/fmtcode.go
@@ -2,59 +2,58 @@ package fmtcode
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"os"
-	"os/exec"
 	"path"
-	"path/filepath"
 	"regexp"
 	"strconv"
-	"strings"
 
 	"github.com/goplus/builder/spx-backend/internal/utils/log"
+	"github.com/goplus/gop/x/format"
 	"golang.org/x/mod/modfile"
 	"golang.org/x/tools/imports"
 )
 
-type FormatError struct {
-	Column int
-	Line   int
-	Msg    string
+// FmtCode Format code
+func FmtCode(ctx context.Context, body string, fixImports bool) *FormatResponse {
+	logger := log.GetReqLogger(ctx)
+	fs, err := splitFiles([]byte(body))
+	if err != nil {
+		logger.Printf("splitFiles failed: %v", err)
+		return newErrorResponse(err)
+	}
+	for _, f := range fs.files {
+		switch {
+		case path.Ext(f) == ".go":
+			out, err := formatGo(f, fs.Data(f), fixImports)
+			if err != nil {
+				logger.Printf("formatGo failed: %v", err)
+				return newErrorResponse(err)
+			}
+			fs.AddFile(f, out)
+		case path.Base(f) == "go.mod":
+			out, err := formatGoMod(f, fs.Data(f))
+			if err != nil {
+				logger.Printf("formatGoMod failed: %v", err)
+				return newErrorResponse(err)
+			}
+			fs.AddFile(f, out)
+		}
+	}
+	return newSuccessResponse(string(fs.Format()))
 }
 
-type FormatResponse struct {
-	Body  string
-	Error FormatError
-}
-
-// extractErrorInfo Format error message
-func extractErrorInfo(errorMsg string) FormatError {
-	// Now a empty FormatError will always be returned, even if there is no error.
-	// TODO: avoid the empty error
-	var de FormatError
-	re := regexp.MustCompile(`:(\d+):(\d+): ([^(\n]+)`)
-	matches := re.FindStringSubmatch(errorMsg)
-	if len(matches) < 4 {
-		de.Msg = errorMsg
-		return de
+func formatGo(file string, data []byte, fixImports bool) ([]byte, error) {
+	if fixImports {
+		// TODO: pass options to imports.Process so it can find symbols in sibling files.
+		return imports.Process(file, data, nil)
 	}
-
-	lineNumber, err := strconv.Atoi(matches[1])
+	out, err := format.GopstyleSource(data, file)
 	if err != nil {
-		de.Msg = errorMsg
-		return de
+		// Unlike imports.Process, format.GopstyleSource does not prefix
+		// the error with the file path. So, do it ourselves here.
+		return nil, fmt.Errorf("%s:%w", file, err)
 	}
-	columnNumber, err := strconv.Atoi(matches[2])
-	if err != nil {
-		de.Msg = errorMsg
-		return de
-	}
-	errorMessage := matches[3]
-	de.Column = columnNumber
-	de.Line = lineNumber
-	de.Msg = errorMessage
-	return de
+	return out, nil
 }
 
 func formatGoMod(file string, data []byte) ([]byte, error) {
@@ -65,106 +64,29 @@ func formatGoMod(file string, data []byte) ([]byte, error) {
 	return f.Format()
 }
 
-// FmtCode Format code
-func FmtCode(ctx context.Context, body string, fixImports bool) (res *FormatResponse) {
-	logger := log.GetReqLogger(ctx)
-	fs, err := splitFiles([]byte(body))
-	if err != nil {
-		logger.Printf("splitFiles failed: %v", err)
-		fmtErr := extractErrorInfo(err.Error())
-		res = &FormatResponse{
-			Body:  "",
-			Error: fmtErr,
-		}
-		return
+type FormatResponse struct {
+	Body  string       `json:",omitempty"`
+	Error *FormatError `json:",omitempty"`
+}
+
+type FormatError struct {
+	Line   int    `json:",omitempty"`
+	Column int    `json:",omitempty"`
+	Msg    string `json:",omitempty"`
+}
+
+func newSuccessResponse(body string) *FormatResponse { return &FormatResponse{Body: body} }
+
+var formatErrorInfoRE = regexp.MustCompile(`:(\d+):(\d+): ([^(\n]+)`)
+
+func newErrorResponse(err error) *FormatResponse {
+	fe := &FormatError{Msg: err.Error()}
+	matches := formatErrorInfoRE.FindStringSubmatch(fe.Msg)
+	if len(matches) > 3 {
+		// The regexp is reliable enough to produce integral line and column numbers.
+		fe.Line, _ = strconv.Atoi(matches[1])
+		fe.Column, _ = strconv.Atoi(matches[2])
+		fe.Msg = matches[3]
 	}
-	for _, f := range fs.files {
-		switch {
-		case path.Ext(f) == ".go":
-			var out []byte
-			var err error
-			in := fs.Data(f)
-			if fixImports {
-				// TODO: pass options to imports.Process so it
-				// can find symbols in sibling files.
-				out, err = imports.Process(f, in, nil)
-			} else {
-				var tmpDir string
-				tmpDir, err = os.MkdirTemp("", "gopformat")
-				if err != nil {
-					logger.Printf("os.MkdirTemp failed: %v", err)
-					fmtErr := extractErrorInfo(err.Error())
-					res = &FormatResponse{
-						Body:  "",
-						Error: fmtErr,
-					}
-					return
-				}
-				defer os.RemoveAll(tmpDir)
-				tmpGopFile := filepath.Join(tmpDir, "prog.gop")
-				if err = os.WriteFile(tmpGopFile, in, 0644); err != nil {
-					logger.Printf("os.WriteFile failed: %v", err)
-					fmtErr := extractErrorInfo(err.Error())
-					res = &FormatResponse{
-						Body:  "",
-						Error: fmtErr,
-					}
-					return
-				}
-				// TODO: include gop as a package depencency, instead of executing gop as binary
-				cmd := exec.Command("gop", "fmt", "-smart", tmpGopFile)
-				//gop fmt returns error result in stdout, so we do not need to handle stderr
-				//err is to check gop fmt return code
-				var fmtErr []byte
-				fmtErr, err = cmd.Output()
-				if err != nil {
-					logger.Printf("cmd.Output failed: %v, %s", err, string(fmtErr))
-					fmtErr := extractErrorInfo(strings.Replace(string(fmtErr), tmpGopFile, "prog.gop", -1))
-					res = &FormatResponse{
-						Body:  "",
-						Error: fmtErr,
-					}
-					return
-				}
-				out, err = os.ReadFile(tmpGopFile)
-				if err != nil {
-					logger.Printf("os.ReadFile failed: %v", err)
-					err = errors.New("interval error when formatting gop code")
-				}
-			}
-			if err != nil {
-				logger.Printf("error encountered: %v", err)
-				errMsg := err.Error()
-				if !fixImports {
-					// Unlike imports.Process, format.Source does not prefix
-					// the error with the file path. So, do it ourselves here.
-					errMsg = fmt.Sprintf("%v:%v", f, errMsg)
-				}
-				fmtErr := extractErrorInfo(errMsg)
-				res = &FormatResponse{
-					Body:  "",
-					Error: fmtErr,
-				}
-				return
-			}
-			fs.AddFile(f, out)
-		case path.Base(f) == "go.mod":
-			out, err := formatGoMod(f, fs.Data(f))
-			if err != nil {
-				logger.Printf("formatGoMod failed: %v", err)
-				fmtErr := extractErrorInfo(err.Error())
-				res = &FormatResponse{
-					Body:  "",
-					Error: fmtErr,
-				}
-				return
-			}
-			fs.AddFile(f, out)
-		}
-	}
-	res = &FormatResponse{
-		Body:  string(fs.Format()),
-		Error: FormatError{},
-	}
-	return
+	return &FormatResponse{Error: fe}
 }
