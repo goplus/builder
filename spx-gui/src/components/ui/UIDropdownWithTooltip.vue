63a915ca4f4ca8eb5a3807a9f254909914588b9d
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-06-13 16:48:50 +0800

    feat: UIDropdownWithTooltip

---
+++ spx-gui/src/components/ui/UIDropdownWithTooltip.vue
@@ -0,0 +1,41 @@
+<template>
+  <UIDropdown
+    trigger="click"
+    :visible="dropdownVisible"
+    @update:visible="handleDropdownVisibleChange"
+  >
+    <template #trigger>
+      <div>
+        <!--
+            TODO:
+            The empty `div` should be avoided. It is now required due to a warning: 
+            `Runtime directive used on component with non-element root node. The directives will not function as intended.`
+           -->
+        <UITooltip :visible="tooltipVisible" @update:visible="handleTooltipVisibleChange">
+          <template #trigger>
+            <slot name="trigger"></slot>
+          </template>
+          <slot name="tooltip-content"></slot>
+        </UITooltip>
+      </div>
+    </template>
+    <slot name="dropdown-content"></slot>
+  </UIDropdown>
+</template>
+
+<script setup lang="ts">
+import { UIDropdown, UITooltip } from '@/components/ui'
+import { ref } from 'vue'
+
+const dropdownVisible = ref(false)
+const tooltipVisible = ref(false)
+
+const handleDropdownVisibleChange = (v: boolean) => {
+  dropdownVisible.value = v
+  tooltipVisible.value = false
+}
+
+const handleTooltipVisibleChange = (v: boolean) => {
+  tooltipVisible.value = v
+}
+</script>
