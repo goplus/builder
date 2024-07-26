c36a7681fc270993965f3e8c403a0d458d9b0717
Tao_ta <745103130@qq.com>
2024-07-26 10:29:20 +0800

    fix: delete bat...

--- tools/fmt/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-set GOOS=js
-set GOARCH=wasm
-
-go build -o static\main.wasm main.go
-
-endlocal
--- tools/ispx/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-set GOOS=js
-set GOARCH=wasm
-
-go build -tags canvas -o main.wasm main.go
-
-endlocal
