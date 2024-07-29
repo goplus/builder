695c3fd0ec0defe3dbff1786b6c053759b8d5bbf
Tsingper <tsingper@outlook.com>
2024-07-29 15:29:00 +0800

    feat: change transfer to tree component

--- spx-gui/src/components/asset/library/LibraryTransfer.vue
+++ spx-gui/src/components/asset/library/LibraryTransfer.vue
@@ -1,31 +1,29 @@
 <template>
-  <n-transfer v-model:value="value" :options="options" />
+  <n-tree
+    block-line
+    :data="data"
+    :default-expanded-keys="defaultExpandedKeys"
+    expand-on-click
+    checkable
+  />
 </template>
 
 <script setup lang="ts">
-import { defineComponent, ref } from 'vue'
+import { ref } from 'vue'
+import type { TreeOption } from 'naive-ui'
 import { categories, type Category } from './category'
-import { useI18n } from '@/utils/i18n'
-import { NTransfer } from 'naive-ui';
+import { type LocaleMessage, useI18n } from '@/utils/i18n'
+import { NTree } from 'naive-ui'
 
-const { t } = useI18n();
-const options = ref(createOptions(categories, t));
-const value = ref<string[]>([]);
-
-
-interface TransferOption {
-  label: string;
-  value: string;
-  children?: TransferOption[];
-}
-
-function createOptions(categories: Category[], t:any): TransferOption[] {
+function createData(categories: Category[], t: (key: LocaleMessage) => string): TreeOption[] {
   return categories.map(category => ({
-    label: t(category.message) as string,
-    value: category.value,
-    children: category.children ? createOptions(category.children, t) : undefined
-  }));
+    label: t(category.message),
+    key: category.value,
+    children: category.children ? createData(category.children, t) : undefined
+  }))
 }
 
-
+const { t } = useI18n()
+const data = ref<TreeOption[]>(createData(categories, t))
+const defaultExpandedKeys = ref<string[]>(['roles', 'backgrounds', 'audio'])
 </script>
