cb5a3aa5653425590544e912aa967ee9230920f3
Tsingper <tsingper@outlook.com>
2024-07-29 15:55:56 +0800

    feat: add personal menu

---
+++ spx-gui/src/components/asset/library/LibraryMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <n-menu :options="menuOptions" @update:value="handleUpdateValue" />
+</template>
+
+<script setup lang="ts">
+import { useMessage,NMenu } from 'naive-ui'
+import type { MenuOption } from 'naive-ui'
+
+const message = useMessage()
+
+function handleUpdateValue(key: string, item: MenuOption) {
+        message.info(`[onUpdate:value]: ${JSON.stringify(key)}`)
+        message.info(`[onUpdate:value]: ${JSON.stringify(item)}`)
+}
+
+const menuOptions: MenuOption[] = [
+  {
+    label: '我的收藏',
+    key: 'my-favorites',
+  },
+  {
+    label: '历史素材',
+    key: 'history',
+  },
+  {
+   label: '已导入素材',
+    key: 'imported',
+  },
+]
+
+</script>
