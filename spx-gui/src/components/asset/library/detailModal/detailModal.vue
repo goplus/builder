366dcf3df2b5537662b79755ad68669af818b6a6
Tsingper <tsingper@outlook.com>
2024-07-31 17:55:59 +0800

    feat: add detail modal

---
+++ spx-gui/src/components/asset/library/detailModal/detailModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <n-button @click="showModal = true">
+    来吧
+  </n-button>
+  <n-modal v-model:show="showModal">
+    <n-card
+      style="width: 600px"
+      title="模态框"
+      :bordered="false"
+      size="huge"
+      role="dialog"
+      aria-modal="true"
+    >
+      <template #header-extra>
+        噢！
+      </template>
+      内容
+      <template #footer>
+        尾部
+      </template>
+    </n-card>
+  </n-modal>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue'
+import { NButton, NModal, NCard } from 'naive-ui'
+
+const showModal=ref(false)
+
+</script>
