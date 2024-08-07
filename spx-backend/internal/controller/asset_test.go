9a5509ea004fc4906cf1429736c776af47433bc1
Tsingper <tsingper@outlook.com>
2024-08-07 09:37:47 +0800

    style: remove test note

--- spx-backend/internal/controller/asset_test.go
+++ spx-backend/internal/controller/asset_test.go
@@ -246,39 +246,6 @@ func TestControllerListAssets(t *testing.T) {
 		assert.Equal(t, "1", assets.Data[0].ID)
 	})
 
-	//t.Run("multiCategory", func(t *testing.T) {
-	//	ctrl, mock, err := newTestController(t)
-	//	require.NoError(t, err)
-	//
-	//	ctx := newContextWithTestUser(context.Background())
-	//	paramsCategory := "cartoon_characters"
-	//	paramsAssetType := model.AssetTypeSprite
-	//	paramsFilesHash := "fake-files-hash"
-	//	paramsIsPublic := model.Personal
-	//	params := &ListAssetsParams{
-	//		Keyword:    "fake",
-	//		Category:   &paramsCategory,
-	//		AssetType:  &paramsAssetType,
-	//		FilesHash:  &paramsFilesHash,
-	//		IsPublic:   &paramsIsPublic,
-	//		OrderBy:    ClickCountDesc,
-	//		Pagination: model.Pagination{Index: 1, Size: 10},
-	//	}
-	//	mock.ExpectQuery(`SELECT COUNT\(\*\) FROM asset WHERE display_name LIKE \? AND category = \? AND asset_type = \? AND files_hash = \? AND is_public = \? AND status != \?`).
-	//		WithArgs("%"+params.Keyword+"%", params.Category, model.AssetTypeSprite, params.FilesHash, model.Public, model.StatusDeleted).
-	//		WillReturnRows(mock.NewRows([]string{"COUNT(1)"}).
-	//			AddRow(1))
-	//	mock.ExpectQuery(`SELECT \* FROM asset WHERE display_name LIKE \? AND category = \? AND asset_type = \? AND files_hash = \? AND is_public = \? AND status != \? ORDER BY click_count DESC LIMIT \?, \? `).
-	//		WithArgs("%"+params.Keyword+"%", params.Category, model.AssetTypeSprite, params.FilesHash, model.Public, model.StatusDeleted, 0, 10).
-	//		WillReturnRows(mock.NewRows([]string{"id", "display_name", "owner"}).
-	//			AddRow(1, "fake-asset", "fake-name"))
-	//	assets, err := ctrl.ListAssets(ctx, params)
-	//	require.NoError(t, err)
-	//	require.NotNil(t, assets)
-	//	assert.Len(t, assets.Data, 1)
-	//	assert.Equal(t, "1", assets.Data[0].ID)
-	//})
-
 	t.Run("DifferentOwner", func(t *testing.T) {
 		ctrl, mock, err := newTestController(t)
 		require.NoError(t, err)
