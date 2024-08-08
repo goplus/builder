9cf28fc9e0e17ec03e7be3f1091dbaa99bd6d7a6
gerrnperl <gerrnperl@outlook.com>
2024-08-08 09:07:51 +0800

    remove mock data

--- spx-gui/src/apis/aigc.ts
+++ spx-gui/src/apis/aigc.ts
@@ -52,68 +52,15 @@ export interface CreateAIImageParams {
 
 const mockAIImage = {
   imageUri: [
-    'kodo://goplus-builder-static-test/files/FvEYel08GXL60vuviffq9sW-9xZs/IMG20230406220355.jpg',
-    'kodo://goplus-builder-static-test/files/FtzNex_e0lYKKn7S52HTG42d9Mm9-6906',
-    'kodo://goplus-builder-static-test/files/FvbBWmTdYLqyx_Mn_wCF6KAhfZXn-5443'
+    // ...
   ]
 }
 
-const mockAISprite = [
-  {
-    files: {
-      'assets/sprites/Samurai/__animation_default_idle-1.png':
-        'kodo://goplus-builder-static-test/files/FhfO2aqtGzCIfIBM003pVb2u2bm8-1951',
-      'assets/sprites/Samurai/__animation_default_idle-2.png':
-        'kodo://goplus-builder-static-test/files/Frzj7kmqyyfOiC0qMLUWWWbtsMuv-1989',
-      'assets/sprites/Samurai/__animation_default_idle-3.png':
-        'kodo://goplus-builder-static-test/files/Fs0V69fiqz3F2bSgsyl-l3DAgkFZ-1994',
-      'assets/sprites/Samurai/__animation_default_idle-4.png':
-        'kodo://goplus-builder-static-test/files/FpMZz1bRKENz4azbIMM7Tz4AFszC-2023',
-      'assets/sprites/Samurai/__animation_default_idle-5.png':
-        'kodo://goplus-builder-static-test/files/FtsVK05enFwnq7t4gsAG_t2BPAPR-2014',
-      'assets/sprites/Samurai/__animation_default_idle-6.png':
-        'kodo://goplus-builder-static-test/files/FoP-MCkmxKE5u4NkzY1CT-86Na66-1985',
-      'assets/sprites/Samurai/__animation_die_dead-1.png':
-        'kodo://goplus-builder-static-test/files/FqP0to8QhEJ6q_KHSG-xLwnuJfox-1651',
-      'assets/sprites/Samurai/__animation_die_dead-2.png':
-        'kodo://goplus-builder-static-test/files/FmH1ocj2U5Yf9vkolJMO-yLkmQDw-1505',
-      'assets/sprites/Samurai/__animation_die_dead-3.png':
-        'kodo://goplus-builder-static-test/files/FigSbdRd5bq-QAGjWZ_i4CTSUEDB-1057',
-      'assets/sprites/Samurai/__animation_fight_attack_1-1.png':
-        'kodo://goplus-builder-static-test/files/Fh0oJdeFwgO2E4vI1sz32rV0Pw0P-1800',
-      'assets/sprites/Samurai/__animation_fight_attack_1-2.png':
-        'kodo://goplus-builder-static-test/files/Fl9sIL2-sOW78iSytYmkRSqm9nK5-1770',
-      'assets/sprites/Samurai/__animation_fight_attack_1-3.png':
-        'kodo://goplus-builder-static-test/files/Fl46Yemx7xn4BjogtaFwIhEqL2z6-1824',
-      'assets/sprites/Samurai/__animation_fight_attack_1-4.png':
-        'kodo://goplus-builder-static-test/files/FupHZVrhWPHPGCNFvfnz52N04M3V-1881',
-      'assets/sprites/Samurai/__animation_fight_attack_1-5.png':
-        'kodo://goplus-builder-static-test/files/Fi6IGob-00asBvUrlllAPMvoj8UC-2019',
-      'assets/sprites/Samurai/__animation_fight_attack_1-6.png':
-        'kodo://goplus-builder-static-test/files/FhD6FEaKO_RZ22jsaNKFS45CBkMv-1869',
-      'assets/sprites/Samurai/__animation_walk_walk-1.png':
-        'kodo://goplus-builder-static-test/files/FugQwRMUp-KATS9dT3g5-rkvpw-7-1792',
-      'assets/sprites/Samurai/__animation_walk_walk-2.png':
-        'kodo://goplus-builder-static-test/files/Fg-Or0cDoT0J10MAUzQnDu9fRPDB-1700',
-      'assets/sprites/Samurai/__animation_walk_walk-3.png':
-        'kodo://goplus-builder-static-test/files/FmLeH0Z5iLaCfI58NMCQ0MYRjBVv-1741',
-      'assets/sprites/Samurai/__animation_walk_walk-4.png':
-        'kodo://goplus-builder-static-test/files/FosR5Xi48mYeW4-aQay0FdnuYyuK-1752',
-      'assets/sprites/Samurai/__animation_walk_walk-5.png':
-        'kodo://goplus-builder-static-test/files/Fp_LMrnWGC5KdIKJq0Xil3RFXF2I-1731',
-      'assets/sprites/Samurai/__animation_walk_walk-6.png':
-        'kodo://goplus-builder-static-test/files/FpHgMK5VOBbve6bXFQdu9RjOWj7S-1670',
-      'assets/sprites/Samurai/__animation_walk_walk-7.png':
-        'kodo://goplus-builder-static-test/files/FmUnJNsLv9QoS7_B5Af6NZ_hjmwQ-1779',
-      'assets/sprites/Samurai/__animation_walk_walk-8.png':
-        'kodo://goplus-builder-static-test/files/FuUFH4RjyGB74ljmuys2wVf46SlK-1759',
-      'assets/sprites/Samurai/default.png':
-        'kodo://goplus-builder-static-test/files/Fh0oJdeFwgO2E4vI1sz32rV0Pw0P-1800',
-      'assets/sprites/Samurai/index.json':
-        'data:application/json,%7B%22heading%22%3A90%2C%22x%22%3A20%2C%22y%22%3A55%2C%22size%22%3A1.89%2C%22rotationStyle%22%3A%22normal%22%2C%22costumeIndex%22%3A0%2C%22visible%22%3Atrue%2C%22isDraggable%22%3Afalse%2C%22costumes%22%3A%5B%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22default%22%2C%22path%22%3A%22default.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_fight_attack_1-1%22%2C%22path%22%3A%22__animation_fight_attack_1-1.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_fight_attack_1-2%22%2C%22path%22%3A%22__animation_fight_attack_1-2.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_fight_attack_1-3%22%2C%22path%22%3A%22__animation_fight_attack_1-3.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_fight_attack_1-4%22%2C%22path%22%3A%22__animation_fight_attack_1-4.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_fight_attack_1-5%22%2C%22path%22%3A%22__animation_fight_attack_1-5.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_fight_attack_1-6%22%2C%22path%22%3A%22__animation_fight_attack_1-6.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_die_dead-1%22%2C%22path%22%3A%22__animation_die_dead-1.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_die_dead-2%22%2C%22path%22%3A%22__animation_die_dead-2.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_die_dead-3%22%2C%22path%22%3A%22__animation_die_dead-3.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_default_idle-1%22%2C%22path%22%3A%22__animation_default_idle-1.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_default_idle-2%22%2C%22path%22%3A%22__animation_default_idle-2.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_default_idle-3%22%2C%22path%22%3A%22__animation_default_idle-3.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_default_idle-4%22%2C%22path%22%3A%22__animation_default_idle-4.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_default_idle-5%22%2C%22path%22%3A%22__animation_default_idle-5.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_default_idle-6%22%2C%22path%22%3A%22__animation_default_idle-6.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_walk_walk-1%22%2C%22path%22%3A%22__animation_walk_walk-1.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_walk_walk-2%22%2C%22path%22%3A%22__animation_walk_walk-2.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_walk_walk-3%22%2C%22path%22%3A%22__animation_walk_walk-3.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_walk_walk-4%22%2C%22path%22%3A%22__animation_walk_walk-4.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_walk_walk-5%22%2C%22path%22%3A%22__animation_walk_walk-5.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_walk_walk-6%22%2C%22path%22%3A%22__animation_walk_walk-6.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_walk_walk-7%22%2C%22path%22%3A%22__animation_walk_walk-7.png%22%7D%2C%7B%22x%22%3A0%2C%22y%22%3A0%2C%22faceRight%22%3A0%2C%22bitmapResolution%22%3A2%2C%22name%22%3A%22__animation_walk_walk-8%22%2C%22path%22%3A%22__animation_walk_walk-8.png%22%7D%5D%2C%22fAnimations%22%3A%7B%22fight%22%3A%7B%22from%22%3A%22__animation_fight_attack_1-1%22%2C%22to%22%3A%22__animation_fight_attack_1-6%22%2C%22duration%22%3A0.6%2C%22anitype%22%3A0%7D%2C%22die%22%3A%7B%22from%22%3A%22__animation_die_dead-1%22%2C%22to%22%3A%22__animation_die_dead-3%22%2C%22duration%22%3A0.6%2C%22anitype%22%3A0%7D%2C%22default%22%3A%7B%22from%22%3A%22__animation_default_idle-1%22%2C%22to%22%3A%22__animation_default_idle-6%22%2C%22duration%22%3A0.6%2C%22anitype%22%3A0%7D%2C%22walk%22%3A%7B%22from%22%3A%22__animation_walk_walk-1%22%2C%22to%22%3A%22__animation_walk_walk-8%22%2C%22duration%22%3A0.8%2C%22anitype%22%3A0%7D%7D%2C%22defaultAnimation%22%3A%22default%22%2C%22animBindings%22%3A%7B%22die%22%3A%22die%22%2C%22step%22%3A%22walk%22%7D%7D'
-    },
-    filesHash: 'h1:TQPF2LZZ2o83Y5J/zK0RGpaqdkw='
-  }
+const mockAISprite: {
+  files: AIGCFiles
+  filesHash: string
+}[] = [
+  // ...
 ]
 
 const mockJobs: Map<string, 'image' | 'sprite'> = new Map()
