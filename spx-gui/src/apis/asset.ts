868cc158f191e1730d4f0cfa0841c88123bda094
gerrnperl <gerrnperl@outlook.com>
2024-08-09 14:56:06 +0800

    feat(spx-gui): Add asset rate API and functionality
    - add `getAssetRate` API to retrieve the rate of an asset
    	and `rateAsset` API to rate an asset (Mock)
    - Add `AssetRate` component to display the asset rating and provide an modal to rate the asset
    - Update the asset details modal for AssetRate component
    
    Note: The rating API is not implemented yet.
    Currently, it will return a random rating that follows a normal distribution.

--- spx-gui/src/apis/asset.ts
+++ spx-gui/src/apis/asset.ts
@@ -131,9 +131,61 @@ export async function getAssetSearchSuggestion(keyword: string, count = 6) {
     orderBy: ListAssetParamOrderBy.TimeAsc
   })
   return {
-    suggestions: Array.from(new Set(assets.data.map(asset => asset.displayName))).slice(0, count)
+    suggestions: Array.from(new Set(assets.data.map((asset) => asset.displayName))).slice(0, count)
   }
   return client.get('/asset/suggest', { keyword, count }) as Promise<{
     suggestions: string[]
   }>
+}
+
+export interface AssetRate {
+  rate: number
+  detail: number[]
+}
+
+/**
+ * Get the rate of an asset
+ * 
+ * WARNING: This API is not implemented in the backend yet. 
+ * Currently, it returns a random rate that follows a normal distribution.
+ * 
+ * @param id 
+ * @returns 
+ */
+export function getAssetRate(id: string): Promise<AssetRate> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const rateCount = Math.floor(Math.random() * 10000)
+      const calcNormDist = (x: number, mean: number, std: number) => {
+        return Math.exp(-Math.pow(x - mean, 2) / (2 * Math.pow(std, 2))) / (std * Math.sqrt(2 * Math.PI))
+      }
+      const targetRate = Math.random() * 4 + 1
+      const distribution = [1, 2, 3, 4, 5].map((rate) => calcNormDist(rate, targetRate, 1))
+      const rates = distribution.map((rate) => Math.floor(rate * rateCount))
+      if (targetRate > 2.5 && targetRate < 3.5 && Math.random() < 0.5) {
+        rates.unshift(rates.pop()!)
+        rates.unshift(rates.pop()!)
+      }
+      resolve({
+        rate: rates.reduce((acc, cur, i) => acc + cur * (i + 1), 0) / rateCount,
+        detail: rates
+      })
+    }, 100)
+  })
+  return client.get(`/asset/${encodeURIComponent(id)}/rate`) as Promise<AssetRate>
+}
+
+/**
+ * Rate an asset
+ * 
+ * WARNING: This API is not implemented in the backend yet.
+ * @param id asset id
+ * @param rate rate value, 1-5
+ * @returns
+ */
+export function rateAsset(id: string, rate: number) {
+  if ([1, 2, 3, 4, 5].indexOf(rate) === -1) {
+    throw new Error('Invalid rate value')
+  }
+  return client.post(`/asset/${encodeURIComponent(id)}/rate`, { rate }) as Promise<void>
 }
