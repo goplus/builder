40c782401462eb690141fc6f4b01ed805f370326
Hanxing Yang <nighca@live.cn>
2024-08-09 15:02:08 +0800

    configurable AIGC feature (#726)

--- spx-gui/src/utils/env.ts
+++ spx-gui/src/utils/env.ts
@@ -15,3 +15,9 @@ export const casdoorConfig = {
   organizationName: import.meta.env.VITE_CASDOOR_ORGANIZATION_NAME as string,
   appName: import.meta.env.VITE_CASDOOR_APP_NAME as string
 }
+
+/**
+ * If we should disable features that rely on AIGC service.
+ * For now AIGC service is not ready for production. We disable it until it's ready.
+ */
+export const disableAIGC = import.meta.env.VITE_DISABLE_AIGC === 'true'
