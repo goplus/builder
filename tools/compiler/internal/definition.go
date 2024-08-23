49ed12c1c99f127c7b0d5d112db01ef859db5602
Tao_ta <745103130@qq.com>
2024-08-23 11:04:44 +0800

    feat: use ident as key to get info from map.

--- tools/compiler/internal/definition.go
+++ tools/compiler/internal/definition.go
@@ -11,21 +11,20 @@ func extractFuncDetails(fun *funcItem, infoList *typesutil.Info) {
 	fun.Name = exprString(token.NewFileSet(), fun.fnExpr)
 	fun.StartPos = int(fun.fnExpr.Pos())
 	fun.EndPos = int(fun.fnExpr.End())
-	for expr, tv := range infoList.Types {
-		if expr == fun.fnExpr {
-			signature := tv.Type.(*types.Signature)
-			parameters := make([]*funcParameter, signature.Params().Len())
-			for i := 0; i < signature.Params().Len(); i++ {
-				parameter := &funcParameter{
-					Name:     signature.Params().At(i).Name(),
-					Type:     signature.Params().At(i).Type().String(),
-					StartPos: int(fun.argsExpr[i].Pos()),
-					EndPos:   int(fun.argsExpr[i].End()),
-				}
-				parameters[i] = parameter
+	if tv, ok := infoList.Types[fun.fnExpr]; ok {
+		signature := tv.Type.(*types.Signature)
+		parameters := make([]*funcParameter, signature.Params().Len())
+		for i := 0; i < signature.Params().Len(); i++ {
+			parameter := &funcParameter{
+				Name:     signature.Params().At(i).Name(),
+				Type:     signature.Params().At(i).Type().String(),
+				StartPos: int(fun.argsExpr[i].Pos()),
+				EndPos:   int(fun.argsExpr[i].End()),
+				//Reload: ,
 			}
-			fun.Parameters = parameters
-			fun.Signature = signature.String()
+			parameters[i] = parameter
 		}
+		fun.Parameters = parameters
+		fun.Signature = signature.String()
 	}
 }
