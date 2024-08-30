689b0f7fa30f61eafe4e3daffbb441ea93505fc8
Callum Chan ( Taota ) <64086092+callme-taota@users.noreply.github.com>
2024-08-30 17:23:08 +0800

    feat: change loading component. (#831)

--- spx-gui/src/components/editor/code-editor/ui/features/chat-bot/ChatLoading.vue
+++ spx-gui/src/components/editor/code-editor/ui/features/chat-bot/ChatLoading.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import UILoading from '@/components/ui/UILoading.vue';
 </script>
 
 <template>
     <div class="chat-loading">
-        <div class="loading-spinner"></div>
+        <UILoading :visible="true"></UILoading>
         <p class="loading-text">{{ $t({ 'zh': '正在生成，请稍候...', 'en': 'Generating, please wait...'}) }}</p>
     </div>
 </template>
@@ -17,26 +18,8 @@
     padding: 20px;
 }
 
-.loading-spinner {
-    width: 30px;
-    height: 30px;
-    border: 3px solid #f3f3f3;
-    border-top: 3px solid #3498db;
-    border-radius: 50%;
-    animation: spin 1s linear infinite;
-}
-
 .loading-text{
     margin-top: 6px;
 }
 
-@keyframes spin {
-    0% {
-        transform: rotate(0deg);
-    }
-
-    100% {
-        transform: rotate(360deg);
-    }
-}
 </style>
