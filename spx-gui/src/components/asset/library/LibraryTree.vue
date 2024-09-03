ce41780eaa5a4dbfe1c0c269bb5ad11385effb74
gerrnperl <gerrnperl@outlook.com>
2024-09-03 16:39:42 +0800

    style: adjust switch icon position and layout in LibraryTree

--- spx-gui/src/components/asset/library/LibraryTree.vue
+++ spx-gui/src/components/asset/library/LibraryTree.vue
@@ -1,33 +1,65 @@
 <template>
   <n-tree
     v-model:checked-keys="checkedKeys"
+    v-model:expanded-keys="expandedKeys"
     block-line
     :data="data"
     expand-on-click
     cascade
     checkable
+    :animated="false"
   />
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { h, ref, watch } from 'vue'
 import type { TreeOption } from 'naive-ui'
 import { categories, type Category } from './category'
 import { type LocaleMessage, useI18n } from '@/utils/i18n'
-import { NTree } from 'naive-ui'
+import { NIcon, NTree } from 'naive-ui'
 import { AssetType } from '@/apis/asset'
-import { useSearchCtx, useSearchResultCtx } from './SearchContextProvider.vue';
+import { useSearchCtx, useSearchResultCtx } from './SearchContextProvider.vue'
+import { ArrowForwardIosRound } from '@vicons/material'
+import { BulbOutlined } from '@vicons/antd'
+import { transform } from 'lodash'
 
 const props = defineProps<{
   type: AssetType
 }>()
 const checkedKeys = ref<string[]>([])
 const searchCtx = useSearchCtx()
+
+const expandedKeys = ref<string[]>([])
+
 function createData(categories: Category[], t: (key: LocaleMessage) => string): TreeOption[] {
   return categories.map((category) => ({
     label: t(category.message),
     key: category.value,
-    children: category.children ? createData(category.children, t) : undefined
+    children: category.children ? createData(category.children, t) : undefined,
+    suffix: category.children
+      ? () => {
+          return h(
+            NIcon,
+            {
+              color: 'var(--text-color)',
+              style: {
+                paddingRight: '10px',
+              }
+            },
+            {
+              default: () =>
+                h(ArrowForwardIosRound, {
+                  style: {
+                    transform: expandedKeys.value.includes(category.value)
+                      ? 'rotate(-90deg)'
+                      : 'rotate(90deg)',
+                    transition: 'transform 0.3s'
+                  }
+                })
+            }
+          )
+        }
+      : undefined
   }))
 }
 
@@ -39,6 +71,7 @@ const data = ref<TreeOption[]>(
     t
   )
 )
+
 watch(
   checkedKeys,
   (newValue) => {
@@ -49,3 +82,13 @@ watch(
 
 //todo: if tabCategory changes, reset category,keyword and selectedValue
 </script>
+
+<style scoped>
+:deep(.n-tree-node-switcher) {
+  display: none;
+}
+:deep(.n-tree-node) {
+  padding: 4px 8px;
+  margin: 0 4px;
+}
+</style>
