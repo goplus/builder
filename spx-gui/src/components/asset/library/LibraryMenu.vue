2dd9d12869dc1b773a0bbf57fee4ef8b858e03f0
gerrnperl <gerrnperl@outlook.com>
2024-09-03 15:46:48 +0800

    feat: Add icons and public option to LibraryMenu.vue

--- spx-gui/src/components/asset/library/LibraryMenu.vue
+++ spx-gui/src/components/asset/library/LibraryMenu.vue
@@ -3,42 +3,58 @@
 </template>
 
 <script setup lang="ts">
-import { NMenu } from 'naive-ui'
+import { NIcon, NMenu } from 'naive-ui'
 import type { MenuOption } from 'naive-ui'
 import { useI18n } from '@/utils/i18n'
 import { ref, watch } from 'vue'
 import { useSearchCtx, type GetListAssetType } from './SearchContextProvider.vue'
+import type { Component } from 'vue'
+import { h } from 'vue'
+import { DownloadForOfflineOutlined, FavoriteBorderRound, HistoryRound } from '@vicons/material'
+import { BulbOutlined } from '@vicons/antd'
 
 const { t } = useI18n()
 
 const emit = defineEmits(['update:value'])
 const searchCtx = useSearchCtx()
 
-const selectedValue = ref<string | null>(null)
+const selectedValue = ref<string>('')
 
 function handleUpdateValue(key: string) {
-  if (selectedValue.value === key) {
-    selectedValue.value = null
-    emit('update:value', '')
-  } else {
+  if (selectedValue.value === key && key !== '') {
+    selectedValue.value = ''
+  }
+  setTimeout(() => {
     selectedValue.value = key
     emit('update:value', selectedValue.value)
-  }
+  }, 0)
 }
 
-const menuOptions: MenuOption[] = [
+const renderIcon = (icon: Component, color?: string) => {
+  return () => h(NIcon, { color }, { default: () => h(icon) })
+}
+
+const menuOptions = [
+  {
+    label: t({ en: 'Public', zh: '公共素材' }),
+    key: '',
+    icon: renderIcon(BulbOutlined),
+  },
   {
     label: t({ en: 'My Favorites', zh: '我的收藏' }),
     key: 'liked',
+    icon: renderIcon(FavoriteBorderRound),
   },
   {
     label: t({ en: 'History', zh: '历史记录' }),
     key: 'history',
+    icon: renderIcon(HistoryRound),
   },
   {
-   label: t({ en: 'Imported', zh: '已导入素材' }),
+    label: t({ en: 'Imported', zh: '已导入素材' }),
     key: 'imported',
-  },
+    icon: renderIcon(DownloadForOfflineOutlined),
+  }
 ]
 
 // if category changes, reset tabCategory and selectedValue
@@ -46,7 +62,7 @@ watch(
   () => searchCtx.category,
   () => {
     searchCtx.tabCategory = 'public'
-    selectedValue.value = null
+    selectedValue.value = ''
   }
 )
 </script>
