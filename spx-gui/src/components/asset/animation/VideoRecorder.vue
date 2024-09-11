fc4bfd316cfe48ede58013f4080a2d7502869f53
gerrnperl <gerrnperl@outlook.com>
2024-09-11 16:41:33 +0800

    feat: update interaction and style of video recorder - add recording time display - merge start and stop recording button to prevent pointless operation - add back, generate animation and export button for recorded - optimize the layout and style of recording and playing interface

--- spx-gui/src/components/asset/animation/VideoRecorder.vue
+++ spx-gui/src/components/asset/animation/VideoRecorder.vue
@@ -1,108 +1,214 @@
 <template>
   <div class="video-recorder">
-    <h2>
-      {{ $t({ en: 'Video Recorder', zh: '视频录制' }) }}
-    </h2>
+    <Transition name="slide-fade" mode="out-in" appear>
+      <div v-if="cameraActive" class="recorder-content record">
+        <!-- seconds -->
+        <div v-if="recording" class="recording-time">
+          {{ $t({ en: 'Recording', zh: '录制中' }) }}: {{ recordingTime }}s
+        </div>
+        <video ref="liveVideoRef" autoplay class="recorder-video record"></video>
 
-    <video ref="videoRef" autoplay></video>
+        <div class="control-btn">
+          <UIButton size="large" @click="recording ? stopRecording() : startRecording()">
+            <NIcon size="large">
+              <StopCircleOutlined v-if="recording" />
+              <SlowMotionVideoOutlined v-else />
+            </NIcon>
+            {{
+              recording
+                ? $t({ en: 'stop recording', zh: '停止录制' })
+                : $t({ en: 'start recording', zh: '开始录制' })
+            }}
+          </UIButton>
+        </div>
+      </div>
+      <div v-else-if="recordedBlobUrl" class="recorder-content playback">
+        <video
+          ref="recordedVideo"
+          controls
+          :src="recordedBlobUrl"
+          class="recorder-video playback"
+        ></video>
+        <div class="control-btn">
+          <UIButton size="large" @click="resetRecorder">
+            <NIcon size="large">
+              <ArrowBackOutlined />
+            </NIcon>
+            {{ $t({ en: 'Return', zh: '返回' }) }}
+          </UIButton>
+          <UIButton size="large" @click="generateAnimation">
+            <NIcon size="large">
+              <RunCircleOutlined />
+            </NIcon>
+            {{ $t({ en: 'Generate', zh: '生成动画' }) }}
+          </UIButton>
+          <UIButton size="large" @click="downloadRecordedVideo">
+            <NIcon size="large">
+              <FileDownloadOutlined />
+            </NIcon>
+            {{ $t({ en: 'Export', zh: '导出' }) }}
+          </UIButton>
+        </div>
+      </div>
+    </Transition>
 
-    <div class="controlBtn">
-      <UIButton size="large" @click="startRecording">
-        {{ $t({ en: 'start recording', zh: '开始录制' }) }}
-      </UIButton>
-      <UIButton size="large" @click="stopRecording">
-        {{ $t({ en: 'stop recording', zh: '停止录制' }) }}
-      </UIButton>
-    </div>
-
-    <video v-if="recordedBlobUrl" ref="recordedVideo" controls :src="recordedBlobUrl"></video>
-
-    <a :href="recordedBlobUrl" download="recorded-video.webm" v-if="recordedBlobUrl">
-      {{ $t({ en: 'Download', zh: '下载' }) }}
-    </a>
+    <Transition name="slide-fade" mode="out-in" appear> </Transition>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, onBeforeUnmount, type Ref} from 'vue';
-import { UIButton } from '@/components/ui';
+import { ref, onMounted, onBeforeUnmount, type Ref, nextTick } from 'vue'
+import { UIButton } from '@/components/ui'
+import { NIcon } from 'naive-ui'
+import {
+  ArrowBackOutlined,
+  FileDownloadOutlined,
+  RunCircleOutlined,
+  SlowMotionVideoOutlined,
+  StopCircleOutlined,
+  VideocamOffOutlined,
+  VideocamOutlined
+} from '@vicons/material'
 
-const mediaStream: Ref<MediaStream | null> = ref(null);
-const mediaRecorder: Ref<MediaRecorder | null> = ref(null);
-const recordedChunks: Ref<Blob[]> = ref([]);
-const recordedBlobUrl: Ref<string | null> = ref(null);
-const videoRef = ref<HTMLVideoElement | null>(null);
-const recordedVideoRef = ref<HTMLVideoElement | null>(null);
+let mediaStream: MediaStream
+let mediaRecorder: MediaRecorder
+let recordedChunks: Blob[] = []
+const cameraActive = ref(false)
+const recording = ref(false)
+const recordingStartedAt = ref<number | null>(null)
+const recordingTime = ref<number | null>(null)
+const recordedBlobUrl = ref<string | null>(null)
+const liveVideoRef = ref<HTMLVideoElement | null>(null)
+const recordedVideoRef = ref<HTMLVideoElement | null>(null)
 
 const startCamera = async () => {
   try {
-    mediaStream.value = await navigator.mediaDevices.getUserMedia({
-      video: true, 
-      audio: true, 
-    });
-    
-    if (videoRef.value) {
-      videoRef.value.srcObject = mediaStream.value;
+    mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false // we don't need audio
+    })
+    cameraActive.value = true
+    await nextTick()
+
+    if (liveVideoRef.value) {
+      liveVideoRef.value.srcObject = mediaStream
     }
   } catch (error) {
-    console.error('无法访问摄像头:', error);
+    console.error('无法访问摄像头:', error)
   }
-};
+}
+
+const stopCamera = () => {
+  if (!mediaStream) {
+    return
+  }
+  mediaStream.getTracks().forEach((track) => track.stop())
+  cameraActive.value = false
+}
 
 const startRecording = () => {
-  if (mediaStream.value) {
-    recordedChunks.value = []; 
-    mediaRecorder.value = new MediaRecorder(mediaStream.value);
-
-    mediaRecorder.value.ondataavailable = (event: BlobEvent) => {
-      if (event.data.size > 0) {
-        recordedChunks.value.push(event.data);
-      }
-    };
-
-    mediaRecorder.value.onstop = () => {
-      const recordedBlob = new Blob(recordedChunks.value, { type: 'video/webm' });
-      recordedBlobUrl.value = URL.createObjectURL(recordedBlob);
-      if (recordedVideoRef.value) {
-        recordedVideoRef.value.src = recordedBlobUrl.value;
-      }
-    };
-
-    mediaRecorder.value.start();
+  if (!mediaStream) {
+    return
   }
-};
+  recordedChunks = []
+  mediaRecorder = new MediaRecorder(mediaStream)
+
+  mediaRecorder.ondataavailable = handleRecorderData
+  mediaRecorder.onstop = handleRecorderStop
+
+  mediaRecorder.start()
+  recording.value = true
+
+  recordingStartedAt.value = Date.now()
+  incrementRecordingTime()
+}
 
 const stopRecording = () => {
-  if (mediaRecorder.value && mediaRecorder.value.state !== 'inactive') {
-    mediaRecorder.value.stop();
+  if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+    mediaRecorder.stop()
+    recording.value = false
   }
-};
+}
+
+const resetRecorder = () => {
+  recordedBlobUrl.value = null
+  startCamera()
+}
+
+const incrementRecordingTime = () => {
+  if (recordingStartedAt.value) {
+    recordingTime.value = Math.floor((Date.now() - recordingStartedAt.value) / 1000)
+  }
+  if (recording.value) {
+    requestAnimationFrame(incrementRecordingTime)
+  }
+}
+
+const handleRecorderData = (event: BlobEvent) => {
+  if (event.data.size > 0) {
+    recordedChunks.push(event.data)
+  }
+}
+
+const handleRecorderStop = () => {
+  stopCamera()
+  const recordedBlob = new Blob(recordedChunks, { type: 'video/webm' })
+  recordedBlobUrl.value = URL.createObjectURL(recordedBlob)
+  if (recordedVideoRef.value) {
+    recordedVideoRef.value.src = recordedBlobUrl.value
+  }
+}
+
+const generateAnimation = () => {
+  // TODO: send recordedBlob to server to generate animation
+}
+
+const downloadRecordedVideo = () => {
+  if (recordedBlobUrl.value) {
+    const a = document.createElement('a')
+    a.href = recordedBlobUrl.value
+    a.download = 'recorded-video.webm'
+    a.click()
+  }
+}
 
 onMounted(() => {
-  startCamera();
-});
+  startCamera()
+})
 
 onBeforeUnmount(() => {
-  if (mediaStream.value) {
-    mediaStream.value.getTracks().forEach((track) => track.stop());
-  }
-});
+  stopCamera()
+})
 </script>
 
 <style scoped>
 .video-recorder {
   text-align: center;
-  margin-top: 20px;
+  min-height: 416px;
+  position: relative;
 }
-.controlBtn {
+
+.recorder-video {
+  width: 100%;
+  max-width: 600px;
+  border-radius: var(--ui-border-radius-1);
+}
+
+.recording-time {
+  position: absolute;
+  top: 4px;
+  left: 4px;
+  width: 7rem;
+  border-radius: var(--ui-border-radius-1);
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 4px;
+}
+
+.control-btn {
   margin: 20px 0;
   display: flex;
   justify-content: center;
-}
-video {
-  width: 80%;
-  max-width: 600px;
-  margin-bottom: 20px;
 }
 button {
   margin: 5px;
