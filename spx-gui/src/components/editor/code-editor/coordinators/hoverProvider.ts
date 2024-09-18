b846d789e46901206dba4c839092a8dcd5a26a33
molinla <61045398+molinla@users.noreply.github.com>
2024-09-18 09:41:52 +0800

    refactor(spx): inputAssistant Provider will filter Token effect in different  code page (#911)
    * feat: add animation in sidebar, refactor sidebar inputAssistant to show whole categories
    
    * feat: filter inputAssistant item in different code page
    
    * refactor: rename some vue ref element variable name
    
    * refactor: refactor tokens property sample, refactor inputAssistant will not merge wasm usages.
    
    * fix: fix some tokens sample
    
    * refactor: refactor `usageEffect2Icon` into 2 functions, add `definitionStructName2Target` to transform wasm definition into TokenUsage Target

--- spx-gui/src/components/editor/code-editor/coordinators/hoverProvider.ts
+++ spx-gui/src/components/editor/code-editor/coordinators/hoverProvider.ts
@@ -3,9 +3,13 @@ import { DocPreviewLevel, Icon } from '@/components/editor/code-editor/EditorUI'
 import { DocAbility } from '@/components/editor/code-editor/document'
 import type { Position } from 'monaco-editor'
 import type { Definition, TokenUsage } from '@/components/editor/code-editor/compiler'
-import type { CoordinatorState } from '@/components/editor/code-editor/coordinators/index'
+import {
+  type CoordinatorState,
+  definitionStructName2Target,
+  usageType2Icon
+} from '@/components/editor/code-editor/coordinators/index'
 import type { TokenWithDoc, UsageWithDoc } from '@/components/editor/code-editor/tokens/types'
-import { usageType2Icon } from '@/components/editor/code-editor/coordinators/index'
+import { usageEffect2Icon } from '@/components/editor/code-editor/coordinators/index'
 import type { Project } from '@/models/project'
 
 export class HoverProvider {
@@ -83,6 +87,7 @@ export class HoverProvider {
       layer: {
         level: DocPreviewLevel.Normal,
         header: {
+          // in this rename case may not have doc usage, use definition usage type to get icon
           icon: usageType2Icon(usage.type),
           declaration: declarationMap[usage.usageID] || usage.declaration
         },
@@ -124,7 +129,14 @@ export class HoverProvider {
       if (usageWithDoc) {
         usages.push({ ...usage, ...usageWithDoc })
       } else {
-        usages.push({ ...usage, id: usage.usageID, effect: definition.structName, doc: '' })
+        // this case is some usage from wasm without pre-defined token usage effect, may use type(func, string, bool) to determine which effect(read, write, listen, func) is
+        usages.push({
+          ...usage,
+          id: usage.usageID,
+          effect: usage.type,
+          target: definitionStructName2Target(definition.structName),
+          doc: ''
+        })
       }
     } else {
       doc.usages.forEach((usage: UsageWithDoc) => {
@@ -134,7 +146,14 @@ export class HoverProvider {
       definition.usages
         .filter((usage) => !docUsageIdSet.has(usage.usageID))
         .forEach((usage) =>
-          usages.push({ ...usage, id: usage.usageID, effect: definition.structName, doc: '' })
+          // this case is some usage from wasm without pre-defined token usage effect, may use type(func, string, bool) to determine which effect(read, write, listen, func) is
+          usages.push({
+            ...usage,
+            id: usage.usageID,
+            effect: usage.type,
+            target: definitionStructName2Target(definition.structName),
+            doc: ''
+          })
         )
     }
 
@@ -143,7 +162,7 @@ export class HoverProvider {
       layer: {
         level: DocPreviewLevel.Normal,
         header: {
-          icon: usageType2Icon(usage.effect),
+          icon: usageEffect2Icon(usage.effect),
           declaration: declarationMap[usage.id] || usage.declaration
         },
         content: usage.doc,
@@ -167,7 +186,17 @@ export class HoverProvider {
     const usageDeclaration: Record<string, string> = {}
     usages.forEach((usage) => {
       const params = usage.params
-        .map((param) => param.name + ' ' + param.type.split('.').pop())
+        .map(
+          (param) =>
+            param.name +
+            ' ' +
+            param.type
+              // if parma type is func() (func(mi *github.com/goplus/spx.MovingInfo))
+              // this line remove: "*"
+              .replace(/\*/g, '')
+              // this line remove: "github.com/goplus/spx."
+              .replace(/(?:[\w/]+\.)+/g, '')
+        )
         .join(', ')
       if (usage.type === 'func') usageDeclaration[usage.usageID] = `${name} (${params})`
     })
