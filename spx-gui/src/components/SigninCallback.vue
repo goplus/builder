d73f9fee72df1905b8ba0c6f1c23b7b952bb3d46
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-09-24 16:33:10 +0800

    consume language from login page (#932)

--- spx-gui/src/components/SigninCallback.vue
+++ spx-gui/src/components/SigninCallback.vue
@@ -5,10 +5,17 @@
 </template>
 <script setup lang="ts">
 import { useUserStore } from '@/stores'
+import { useI18n } from '@/utils/i18n'
 
 const userStore = useUserStore()
+const i18n = useI18n()
 
 try {
+  const params = new URLSearchParams(location.search)
+  const lang = params.get('language')
+  if (lang === 'en' || lang === 'zh') {
+    i18n.setLang(lang)
+  }
   await userStore.consumeCurrentUrl()
 } finally {
   window.location.replace('/')
