c93962f74fb4a7dd8fea048a883e936c0fabd0ac
nighca <nighca@live.cn>
2024-09-26 10:42:00 +0800

    move SharePage to project page

--- spx-gui/src/pages/community/project.vue
+++ spx-gui/src/pages/community/project.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>Project {{ owner }}/{{ name }}</div>
+  <RunnerContainer v-if="project != null" mode="share" :project="project" />
+  <UILoading :visible="project == null" cover />
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { useAsyncComputed } from '@/utils/utils'
+import { Project } from '@/models/project'
+import RunnerContainer from '@/components/project/runner/RunnerContainer.vue'
+import { UILoading } from '@/components/ui'
+
+const props = defineProps<{
   owner: string
   name: string
 }>()
+
+const project = useAsyncComputed(async () => {
+  const p = new Project()
+  await p.loadFromCloud(props.owner, props.name)
+  return p
+})
 </script>
