2aac9aa1dde7e0cbd4cfb93d051b88b8101de453
Hanxing Yang <nighca@live.cn>
2024-09-29 10:14:12 +0800

    layout for community pages (#949)

---
+++ spx-gui/src/components/community/TopNav.vue
@@ -0,0 +1,8 @@
+<template>
+  <TopNav :project="null"></TopNav>
+  <!-- TODO -->
+</template>
+
+<script setup lang="ts">
+import TopNav from '@/components/top-nav/TopNav.vue'
+</script>
--- spx-gui/src/pages/community/index.vue
+++ spx-gui/src/pages/community/index.vue
@@ -1,4 +1,19 @@
 <template>
-  <header>Community Navbar</header>
-  <router-view />
+  <div class="wrapper">
+    <TopNav></TopNav>
+    <router-view />
+  </div>
 </template>
+
+<script setup lang="ts">
+import TopNav from '@/components/community/TopNav.vue'
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  background-color: var(--ui-color-grey-300);
+}
+</style>
