298cea04353245705ed85da594b997973f27a636
nighca <nighca@live.cn>
2024-10-24 19:23:25 +0800

    use displayName instead of name for user

--- spx-gui/src/components/community/project/RemixedFrom.vue
+++ spx-gui/src/components/community/project/RemixedFrom.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { parseRemixSource, stringifyRemixSource } from '@/apis/project'
+import { parseRemixSource } from '@/apis/project'
 import { getProjectPageRoute } from '@/router'
+import { useUser } from '@/stores/user'
 import RouterUILink from '@/components/common/RouterUILink.vue'
 
 const props = defineProps<{
@@ -9,13 +10,21 @@ const props = defineProps<{
 }>()
 
 const remixedFrom = computed(() => parseRemixSource(props.remixedFrom))
+const { data: owner } = useUser(() => remixedFrom.value.owner)
+const title = computed(() => {
+  if (owner.value == null) return null
+  return {
+    en: `${remixedFrom.value.project} created by ${owner.value.displayName}`,
+    zh: `${remixedFrom.value.project}，由 ${owner.value.displayName} 创建`
+  }
+})
 </script>
 
 <template>
   <p class="remixed-from">
     {{ $t({ en: 'Remixed from', zh: '改编自' }) }}
-    <RouterUILink :to="getProjectPageRoute(remixedFrom.owner, remixedFrom.project)" type="boring">
-      {{ stringifyRemixSource(remixedFrom.owner, remixedFrom.project) }}
+    <RouterUILink :to="getProjectPageRoute(remixedFrom.owner, remixedFrom.project)" type="boring" :title="$t(title)">
+      {{ remixedFrom.project }}
     </RouterUILink>
   </p>
 </template>
