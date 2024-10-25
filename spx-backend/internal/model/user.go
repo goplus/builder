a2b6857163a63389babd5b7b437b6400dfdaed36
Aofei Sheng <aofei@aofeisheng.com>
2024-10-25 15:07:12 +0800

    fix(spx-backend): do nothing on `username` conflict in `model.FirstOrCreateUser`
    Signed-off-by: Aofei Sheng <aofei@aofeisheng.com>

--- spx-backend/internal/model/user.go
+++ spx-backend/internal/model/user.go
@@ -2,11 +2,12 @@ package model
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/casdoor/casdoor-go-sdk/casdoorsdk"
+	"github.com/go-sql-driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 // User is the model for users.
@@ -46,6 +47,8 @@ func (User) TableName() string {
 	return "user"
 }
 
+const userDuplicateUsernameErrorMessage = "Duplicate entry: An active record with same username already exists"
+
 func (User) AfterMigrate(tx *gorm.DB) error {
 	for _, sql := range []string{
 		"DROP TRIGGER IF EXISTS trg_user_before_insert",
@@ -61,7 +64,7 @@ BEGIN
 		AND deleted_at IS NULL
 	) THEN
 		SIGNAL SQLSTATE '45000'
-		SET MESSAGE_TEXT = 'Duplicate entry: An active record with same username already exists';
+		SET MESSAGE_TEXT = '` + userDuplicateUsernameErrorMessage + `';
 	END IF;
 END
 		`,
@@ -81,7 +84,7 @@ BEGIN
 		AND id != NEW.id
 	) THEN
 		SIGNAL SQLSTATE '45000'
-		SET MESSAGE_TEXT = 'Duplicate entry: An active record with same username already exists';
+		SET MESSAGE_TEXT = '` + userDuplicateUsernameErrorMessage + `';
 	END IF;
 END
 		`,
@@ -91,6 +94,15 @@ END
 		}
 	}
 	return nil
+}
+
+// isUserDuplicateUsernameError reports whether the err is a duplicate username error.
+func isUserDuplicateUsernameError(err error) bool {
+	var mysqlError *mysql.MySQLError
+	if errors.As(err, &mysqlError) {
+		return mysqlError.Number == 1644 && mysqlError.Message == userDuplicateUsernameErrorMessage
+	}
+	return false
 }
 
 // FirstOrCreateUser gets or creates a user.
@@ -103,13 +115,11 @@ func FirstOrCreateUser(ctx context.Context, db *gorm.DB, casdoorUser *casdoorsdk
 			DisplayName: casdoorUser.DisplayName,
 			Avatar:      casdoorUser.Avatar,
 		}).
-		Clauses(clause.OnConflict{
-			Columns:   []clause.Column{{Name: "username"}},
-			DoNothing: true,
-		}).
 		FirstOrCreate(&mUser).
 		Error; err != nil {
-		return nil, fmt.Errorf("failed to get/create user %s: %w", casdoorUser.Name, err)
+		if !isUserDuplicateUsernameError(err) {
+			return nil, fmt.Errorf("failed to get/create user %s: %w", casdoorUser.Name, err)
+		}
 	}
 	if mUser.ID == 0 {
 		// Unfortunatlly, MySQL doesn't support the RETURNING clause.
