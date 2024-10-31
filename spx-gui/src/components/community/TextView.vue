9f88d1c84e2e23ca7ead21d32d4b406eb1623f9c
Hanxing Yang <nighca@live.cn>
2024-10-31 15:47:06 +0800

    preserve line breaks in text content (#1061)

---
+++ spx-gui/src/components/community/TextView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+  placeholder?: string
+}>()
+
+// TODO: more feature, see details in https://github.com/goplus/builder/issues/1060
+</script>
+
+<template>
+  <div class="text-view">
+    <template v-if="!!text">{{ text }}</template>
+    <template v-else>{{ placeholder ?? '&nbsp;' }}</template>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.text-view {
+  overflow-y: auto;
+  white-space: pre-wrap;
+  word-break: break-word;
+}
+</style>
