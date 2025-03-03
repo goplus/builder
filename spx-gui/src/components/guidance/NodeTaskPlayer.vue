34f24684c6df22680c187a02806b8b3af9042344
HaoQiuji-Pavel <qiujihao030806@outlook.com>
2025-03-03 12:02:05 +0800

    feat: add NodeTaskPlayer empty template

---
+++ spx-gui/src/components/guidance/NodeTaskPlayer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="node-task-player">
+    <!-- 这里未来将会渲染 StepPlayer 组件 -->
+    <p>NodeTaskPlayer 开发中...</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { NodeTask } from './common/types';
+
+// 组件的对外接口
+const props = defineProps<{ nodeTask: NodeTask }>();
+
+const emit = defineEmits<{ nodeTaskCompleted: [] }>();
+
+// TODO
+</script>
+
+<style scoped>
+
+</style>
