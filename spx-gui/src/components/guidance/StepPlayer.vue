99ee928cfec8824966ea2bfeb1f1a6e68a7e2444
neverbiasu <1751162157@qq.com>
2025-03-06 17:50:08 +0800

    feat: Add filter to all resources

--- spx-gui/src/components/guidance/StepPlayer.vue
+++ spx-gui/src/components/guidance/StepPlayer.vue
@@ -21,7 +21,27 @@ onMounted(() => {
   if (props.step.isApiControl) {
     filter.setFilter('apiReference', true, props.step.apis)
   }
-  // ... same logic for other types of steps
+  if (props.step.isAssetControl) {
+    filter.setFilter('asset', true, props.step.assets)
+  }
+  if (props.step.isSpriteControl) {
+    filter.setFilter('sprite', true, props.step.sprite)
+  }
+  if (props.step.isSoundControl) {
+    filter.setFilter('sound', true, props.step.sound)
+  }
+  if (props.step.isCostumeControl) {
+    filter.setFilter('costume', true, props.step.costume)
+  }
+  if (props.step.isAnimationControl) {
+    filter.setFilter('animation', true, props.step.animation)
+  }
+  if (props.step.isWidgetControl) {
+    filter.setFilter('widget', true, props.step.widget)
+  }
+  if (props.step.isBackdropControl) {
+    filter.setFilter('backdrop', true, props.step.backdrop)
+  }
 })
 
 onBeforeUnmount(() => {
