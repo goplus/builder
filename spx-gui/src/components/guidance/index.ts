3f4f87253697831be026edbe48bfca002c6e2bf3
HaoQiuji-Pavel <qiujihao030806@outlook.com>
2025-03-30 15:14:02 +0800

    fix: fix some bugs and optimize UI

--- spx-gui/src/components/guidance/index.ts
+++ spx-gui/src/components/guidance/index.ts
@@ -5,16 +5,7 @@ import type { Project } from '@/models/project'
 export function useCreateWholeStoryLineProject() {
   const modal = useModal(WholeStoryLineProjectCreateModal)
 
-<<<<<<< HEAD
   return async function createWholeStoryLineProject(title: LocaleMessage, project: Project) {
     return modal({ title, project })
-=======
-  return async function createWholeStoryLineProject(title: LocaleMessage, project?: Project) {
-    let projectFile: File | undefined
-    if (project) {
-      projectFile = await project.exportGbpFile()
-    }
-    return modal({ title, projectFile })
->>>>>>> 8cc5fbd9cf62e5751704dace34c87da2a0e0a2f6
   }
 }
