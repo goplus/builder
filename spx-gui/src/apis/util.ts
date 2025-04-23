acb999c01e5e0b35b6aab23402da2f4d4673e4ed
Aofei Sheng <aofei@aofeisheng.com>
2025-04-23 11:13:46 +0800

    refactor(apis/util): work around CDN back-to-origin slowness in `makeObjectUrls` (#1599)
    Fixes #1598
    
    Signed-off-by: Aofei Sheng <aofei@aofeisheng.com>

--- spx-gui/src/apis/util.ts
+++ spx-gui/src/apis/util.ts
@@ -2,6 +2,7 @@
  * @desc util-related APIs of spx-backend
  */
 
+import { usercontentBaseUrl } from '@/utils/env'
 import { client, type UniversalUrl, type UniversalToWebUrlMap } from './common'
 
 export type UpInfo = {
@@ -22,6 +23,17 @@ export function getUpInfo() {
 }
 
 export async function makeObjectUrls(objects: UniversalUrl[]): Promise<UniversalToWebUrlMap> {
-  const result = (await client.post('/util/fileurls', { objects: objects })) as { objectUrls: UniversalToWebUrlMap }
-  return result.objectUrls
+  return workAroundIssue1598(objects)
+
+  // const result = (await client.post('/util/fileurls', { objects: objects })) as { objectUrls: UniversalToWebUrlMap }
+  // return result.objectUrls
+}
+
+/** Workaround for https://github.com/goplus/builder/issues/1598 */
+function workAroundIssue1598(objects: UniversalUrl[]): UniversalToWebUrlMap {
+  return objects.reduce((map, universalUrl) => {
+    const url = new URL(universalUrl)
+    map[universalUrl] = url.protocol === 'kodo:' ? usercontentBaseUrl + url.pathname + url.search : universalUrl
+    return map
+  }, {} as UniversalToWebUrlMap)
 }
