05e5f3d8db5730f730bf7ad05651c04263b7eeee
Hanxing Yang <nighca@live.cn>
2025-04-24 16:30:57 +0800

    Upgrade spx to v2.0.4 (#1604)

--- spx-gui/install-spx.sh
+++ spx-gui/install-spx.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Version of spx, keep in sync with the version in `.env`.
-SPX_VERSION=2.0.3
+SPX_VERSION=2.0.4
 SPX_NAME=spx_${SPX_VERSION}
 SPX_FILE_NAME=${SPX_NAME}.zip
 SPX_FILE_URL=https://github.com/goplus/godot/releases/download/spx${SPX_VERSION}/spx_web.zip
