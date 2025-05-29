3a5956eeeba8ddb8fe4536a3dce225239c9641fb
nighca <nighca@live.cn>
2025-05-29 16:16:05 +0800

    Upgrade spx to v2.0.0-beta2

--- spx-gui/install-spx.sh
+++ spx-gui/install-spx.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Version of spx, keep in sync with the version in `.env`.
-SPX_VERSION=2.0.13
+SPX_VERSION=2.0.0-beta2
 SPX_NAME="spx_${SPX_VERSION}"
 SPX_FILE_NAME="${SPX_NAME}.zip"
-SPX_FILE_URL="https://github.com/goplus/godot/releases/download/spx${SPX_VERSION}/spx_web.zip"
+SPX_FILE_URL="https://github.com/goplus/spx/releases/download/v${SPX_VERSION}/spx_web.zip"
 
 wget -O "${SPX_FILE_NAME}" "${SPX_FILE_URL}"
 unzip -o "${SPX_FILE_NAME}" -d "./public/${SPX_NAME}"
