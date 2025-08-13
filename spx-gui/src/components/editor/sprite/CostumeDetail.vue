04165af7778dee0aeb8647e69d4de5ab9a260619
AvenJi0320 <pychi0320@gmail.com>
2025-08-13 16:45:13 +0800

    feat(spx-gui):add modify privew & change img save func

--- spx-gui/src/components/editor/sprite/CostumeDetail.vue
+++ spx-gui/src/components/editor/sprite/CostumeDetail.vue
@@ -2,7 +2,7 @@
   <EditorItemDetail :name="costume.name" @rename="handleRename">
     <div class="painter-wrapper">
       <CheckerboardBackground class="background" />
-      <Painter class="painter" :img-src="imgSrc" :img-loading="imgLoading" />
+      <Painter class="painter" :img-src="imgSrc" :img-loading="imgLoading" @svg-change="handleSvgChange" />
     </div>
   </EditorItemDetail>
 </template>
@@ -16,6 +16,8 @@ import EditorItemDetail from '../common/EditorItemDetail.vue'
 import CheckerboardBackground from './CheckerboardBackground.vue'
 import Painter from '@/components/editor/common/painter/painter.vue'
 import { useFileUrl } from '@/utils/file'
+import { fromText } from '@/models/common/file'
+import { useEditorCtx } from '../EditorContextProvider.vue'
 
 const props = defineProps<{
   costume: Costume
@@ -29,6 +31,34 @@ const handleRename = useMessageHandle(() => renameCostume(props.costume), {
 }).fn
 
 const [imgSrc, imgLoading] = useFileUrl(() => props.costume.img)
+
+// 在 setup 阶段获取 editorCtx，避免在异步函数中调用
+const editorCtx = useEditorCtx()
+
+async function handleSvgChange(svg: string) {
+  console.log('handleSvgChange 被调用，SVG 长度:', svg?.length)
+  // 用导出的 SVG 替换 costume.img
+  // 继承原文件名的基础名，改后缀为 .svg
+  const name = props.costume.name.replace(/[\\/\\:*?\"<>|]/g, '-') + '.svg'
+  const file = fromText(name, svg, { type: 'image/svg+xml' })
+  console.log('创建新文件:', name, '类型:', file.type)
+  
+  // 保存原始的分辨率设置
+  const originalBitmapResolution = props.costume.bitmapResolution
+  
+  // 历史记录包装
+  await editorCtx.project.history.doAction(
+    { name: { en: 'Update costume image', zh: '更新造型图片' } },
+    () => {
+      console.log('执行 setImg')
+      props.costume.setImg(file)
+      // 保持原始分辨率，不要强制改为1
+      props.costume.setBitmapResolution(originalBitmapResolution)
+      console.log('setImg 完成')
+    }
+  )
+  console.log('handleSvgChange 完成')
+}
 </script>
 
 <style lang="scss" scoped>
