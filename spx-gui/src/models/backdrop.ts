1b639d8cf4e2735eb9c4b8b30736c561f12d4a0d
Hanxing Yang <nighca@live.cn>
2025-08-14 11:58:33 +0800

    Remove inheritance from Backdrop to Costume (#1970)

--- spx-gui/src/models/backdrop.ts
+++ spx-gui/src/models/backdrop.ts
@@ -1,18 +1,23 @@
 import { reactive } from 'vue'
-import { resolve } from '@/utils/path'
+import { nanoid } from 'nanoid'
+import { extname, resolve } from '@/utils/path'
 import { adaptImg } from '@/utils/spx'
 import type { File, Files } from './common/file'
 import { getBackdropName, validateBackdropName } from './common/asset-name'
 import type { AssetMetadata } from './common/asset'
 import type { Stage } from './stage'
-import { type CostumeInits, type RawCostumeConfig, Costume } from './costume'
 
-export type BackdropInits = CostumeInits & {
+export type BackdropInits = {
+  id?: string
+  bitmapResolution?: number
   assetMetadata?: AssetMetadata
 }
 
-export type RawBackdropConfig = RawCostumeConfig & {
+export type RawBackdropConfig = Omit<BackdropInits, 'id' | 'assetMetadata'> & {
+  builder_id?: string
   builder_assetMetadata?: AssetMetadata
+  name?: string
+  path?: string
 }
 
 const backdropAssetPath = 'assets'
@@ -22,17 +27,29 @@ export type BackdropExportLoadOptions = {
   includeAssetMetadata?: boolean
 }
 
-// Backdrop is almost the same as Costume
-export class Backdrop extends Costume {
+export class Backdrop {
+  id: string
+
   stage: Stage | null = null
   setStage(stage: Stage | null) {
     this.stage = stage
   }
 
+  name: string
   setName(name: string) {
     const err = validateBackdropName(name, this.stage)
     if (err != null) throw new Error(`invalid name ${name}: ${err.en}`)
     this.name = name
+  }
+
+  img: File
+  setImg(img: File) {
+    this.img = img
+  }
+
+  bitmapResolution: number
+  setBitmapResolution(bitmapResolution: number) {
+    this.bitmapResolution = bitmapResolution
   }
 
   assetMetadata: AssetMetadata | null
@@ -41,7 +58,10 @@ export class Backdrop extends Costume {
   }
 
   constructor(name: string, file: File, inits?: BackdropInits) {
-    super(name, file, inits)
+    this.name = name
+    this.img = file
+    this.bitmapResolution = inits?.bitmapResolution ?? 1
+    this.id = inits?.id ?? nanoid()
     this.assetMetadata = inits?.assetMetadata ?? null
     return reactive(this) as this
   }
@@ -59,7 +79,7 @@ export class Backdrop extends Costume {
   }
 
   static load(
-    { name, path, builder_id: id, ...inits }: RawBackdropConfig,
+    { name, path, builder_id: id, builder_assetMetadata: assetMetadata, ...inits }: RawBackdropConfig,
     files: Files,
     { includeId = true, includeAssetMetadata: includeMetadata = true }: BackdropExportLoadOptions = {}
   ) {
@@ -70,7 +90,7 @@ export class Backdrop extends Costume {
     return new Backdrop(name, file, {
       ...inits,
       id: includeId ? id : undefined,
-      assetMetadata: includeMetadata ? inits.builder_assetMetadata : undefined
+      assetMetadata: includeMetadata ? assetMetadata : undefined
     })
   }
 
@@ -78,10 +98,18 @@ export class Backdrop extends Costume {
     RawBackdropConfig,
     Files
   ] {
-    const [costumeConfig, files] = super.export({ basePath: backdropAssetPath, includeId })
-    let config: RawBackdropConfig = costumeConfig
+    const filename = this.name + extname(this.img.name)
+    const config: RawBackdropConfig = {
+      bitmapResolution: this.bitmapResolution,
+      name: this.name,
+      path: filename
+    }
+    if (includeId) config.builder_id = this.id
     if (includeMetadata && this.assetMetadata != null) {
-      config = { ...config, builder_assetMetadata: this.assetMetadata }
+      config.builder_assetMetadata = this.assetMetadata
+    }
+    const files = {
+      [resolve(backdropAssetPath, filename)]: this.img
     }
     return [config, files]
   }
