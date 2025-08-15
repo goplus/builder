12e19383835de9a1df7af59f5ab09ba6b52281ab
AvenJi0320 <pychi0320@gmail.com>
2025-08-15 15:05:38 +0800

    feat(painter): add 5 placeholder tools with improved layout

---
+++ spx-gui/src/components/editor/common/painter/components/rectangle_tool.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="rectangle-tool">
+    <!-- TODO: 实现矩形工具功能 -->
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO: 实现矩形工具逻辑
+</script>
+
+<style scoped>
+.rectangle-tool {
+  /* TODO: 添加矩形工具样式 */
+}
+</style>
---
+++ spx-gui/src/components/editor/common/painter/components/text_tool.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-tool">
+    <!-- TODO: 实现插入文本工具功能 -->
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO: 实现插入文本工具逻辑
+</script>
+
+<style scoped>
+.text-tool {
+  /* TODO: 添加插入文本工具样式 */
+}
+</style>
