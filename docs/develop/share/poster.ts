3803d7dfaa167da3222edaa321f813e93c1ff19f
WJH <3506456886@qq.com>
2025-08-19 16:24:16 +0800

    feat: add screenShot Module architecture TS file

--- docs/develop/share/poster.ts
+++ docs/develop/share/poster.ts
@@ -1,13 +1,13 @@
 import { ref, onMounted, watch, nextTick, computed } from 'vue' // 通过父组件获取填充文字以及图片
 import { generateProjectQRcode } from './qrcode'
-import { generateJumpToQRcode } from './qrcode' // './第三方平台'
 import html2canvas from 'html2canvas'
 // import { UIIcon } from '@/components/ui' 考虑到后面可能无法引用XBuilder组件库
+// 海报中不需要跳转到第三方平台 import { generateJumpToQRcode } from './qrcode' // './第三方平台'
 
-interface PosterProps {
+interface posterProps {
     width?: number
     height?: number
-    imgSrc?: string
+    imgSrc?: string // 本来想通过直接嵌入screenShot的canvas，但是为了兼容性还是选择图片
     title?: string
     owner?: string
     description?: string
@@ -20,9 +20,9 @@ interface PosterProps {
     jumpToUrl?: string
 }
 
-async function posterResult(props: PosterProps): Promise<string> {
+async function posterResult(props: posterProps): Promise<string> {
     const posterElement = document.createElement('div')
-    // 在这里处理这个DOM节点 posterElement.className = 'poster-container'等等
+    // 在这里处理这个DOM节点 posterElement.className = 'poster-container'、填入props信息、二维码等等
     const canvas = await html2canvas(posterElement,{
         width: props.width || 600,
         height: props.height || 800
