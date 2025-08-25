2641ee17d3b05b70f26bf9eaf2cd447eb25bdf3a
Lotterng <759932827@qq.com>
2025-08-25 18:10:33 +0800

    refactor:修改逻辑

--- docs/develop/share/directShare.ts
+++ docs/develop/share/directShare.ts
@@ -1,5 +1,15 @@
-import { PlatformShare, SocialPlatforms } from "./sharePlatform"
+import { PlatformConfig, SocialPlatformConfigs } from "./platformShare"
 import { createPoster } from "./poster"
+import { defineProps } from "vue"
+/**
+ * 定义组件的props
+ */
+const props = defineProps<{
+    projectData: {
+        url: string
+    },
+    poster: File
+}>()
 /**
  * 模拟qrcode返回的DataURL
  */
@@ -8,10 +18,8 @@ const qrcode = {
         return `data:image/png;base64,${url}`
     }
 }
-// 默认项目链接
-const projectUrl = 'https://www.qiniu.com'
 // 默认选择第一个平台
-const selectPlatform = SocialPlatforms[0]
+const selectPlatform = SocialPlatformConfigs[0]
 // 二维码地址 - 默认空字符串
 let DataURL: string = ''
 
@@ -29,12 +37,12 @@ const posterData = await createPoster({
     }
 })
 // 模拟平台切换
-async function handPlatformChange(platform: PlatformShare) {
+async function handPlatformChange(platform: PlatformConfig) {
     if (platform.shareType.supportProject && platform.shareFunction.shareURL) {
-        DataURL = qrcode.toDataURL(await platform.shareFunction.shareURL(projectUrl))
+        DataURL = qrcode.toDataURL(await platform.shareFunction.shareURL(props.projectData.url))
     }
     else if (platform.shareType.supportPoster && platform.shareFunction.shareImage) {
-        DataURL = qrcode.toDataURL(await platform.shareFunction.shareImage(poster))
+        DataURL = qrcode.toDataURL(await platform.shareFunction.shareImage(posterData))
     }
     else {
         DataURL = ''
