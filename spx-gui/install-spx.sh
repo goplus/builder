9a4d65289c5144213af1b35a7070bd437bf65178
Aofei Sheng <aofei@aofeisheng.com>
2025-08-27 14:12:53 +0800

    chore(deps): bump spx and xgolsw for API redesign
    Updates goplus/spx#785
    
    Signed-off-by: Aofei Sheng <aofei@aofeisheng.com>

--- spx-gui/install-spx.sh
+++ spx-gui/install-spx.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Version of spx, keep in sync with the version in `.env`.
-SPX_VERSION=2.0.0-pre.4
+SPX_VERSION=2.0.0-pre.7
 SPX_NAME="spx_${SPX_VERSION}"
 SPX_FILE_NAME="${SPX_NAME}.zip"
 SPX_FILE_URL="https://github.com/goplus/spx/releases/download/v${SPX_VERSION}/spx_web.zip"
