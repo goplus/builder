600395bbe71471eac5de7fc4bac2da1230ee9aa0
Lotterng <759932827@qq.com>
2025-09-02 11:54:04 +0800

    refactor:完善directsharing模块接口逻辑

--- docs/develop/share/module_ProjectDirectSharing.ts
+++ docs/develop/share/module_ProjectDirectSharing.ts
@@ -2,16 +2,18 @@
  * 直接分享组件
 */
 // 导入必要的类型和函数
-import { PlatformConfig } from "./module_platformShare"
-import Poster from "./module_poster"
 import { defineProps, ref, defineEmits } from "vue"
+import { PlatformConfig } from "./module_platformShare"
+import { ProjectData } from '@/apis/project'
+import platformSelector from './platformSelector.vue'
+import Poster from "./module_poster"
+
 /**
  * 定义组件的props
  */
 const props = defineProps<{
-    projectData: {
-        // 项目数据
-    }
+    projectData: ProjectData
+    visible: boolean
 }>()
 
 const emit = defineEmits<{
