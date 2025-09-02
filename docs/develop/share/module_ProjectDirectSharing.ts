bb476f4dad33a6ba42b623a513cb6b85ac302aed
ceilf <3506456886@qq.com>
2025-09-02 15:09:17 +0800

    fix: link

--- docs/develop/share/module_ProjectDirectSharing.ts
+++ docs/develop/share/module_ProjectDirectSharing.ts
@@ -3,7 +3,7 @@
 */
 // 导入必要的类型和函数
 import { PlatformConfig } from "./module_platformShare"
-import Poster from "./module_poster"
+import Poster from "./module_ProjectPoster"
 import { defineProps, ref, defineEmits } from "vue"
 /**
  * 定义组件的props
