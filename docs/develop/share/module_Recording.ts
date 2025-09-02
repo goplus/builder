b8085079da9a7a9a9ea43bc3e58707c6c966ea1c
hwcctn <2738268497@qq.com>
2025-09-02 17:24:38 +0800

    feat:recoding doc and mobileKeyboard

--- docs/develop/share/module_Recording.ts
+++ docs/develop/share/module_Recording.ts
@@ -1,12 +1,16 @@
+// 它是一个路由页面，使用的话就是用
+//  <RouterLink to="/recording/:id">Recording</RouterLink>
+// 路由会自动把id作为props传给RecordingPage
+export declare function RecordingPage(props: { id: string });
+
 import { RecordingData } from "./module_RecordingApis";
 
 /**
  * Context (list) where the record item is used
  */
 export type RecordingItemContext = "public" | "mine";
-
 /**
- * Record Item component for displaying individual recordings
+ * Recording Item component for displaying individual recordings
  *
  * Supports different contexts with varying functionality:
  * - public: Public recording listings (view-only)
