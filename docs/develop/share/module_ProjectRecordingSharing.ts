db5c0605c10e89963f1567853770834afb81d070
ceilf <3506456886@qq.com>
2025-09-02 15:29:15 +0800

    fix: names

--- docs/develop/share/module_ProjectRecordingSharing.ts
+++ docs/develop/share/module_ProjectRecordingSharing.ts
@@ -4,9 +4,9 @@
  * @Date: 2025.9.1
  -->
 import { ref, computed, defineProps, defineEmits } from 'vue'
-import { directShare, shareVideo, SocialPlatformConfigs } from './module_PlatformShare'
-import type { RecordData } from './module_RecordingApis'
-import type { PlatformShare } from './module_PlatformShare'
+import { directShare, shareVideo, SocialPlatformConfigs } from './module_platformShare'
+import type { RecordingData } from './module_RecordingApis'
+import type { PlatformShare } from './module_platformShare'
 
 const props = defineProps<{
     recording: Promise<RecordData>,
