5a61813bd205c341ab4e607b580ce49e10ccd28b
WJH <3506456886@qq.com>
2025-09-04 12:40:36 +0800

    feat: poster

---
+++ spx-gui/src/components/project/sharing/ProjectPoster.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"> // expose、defineExpose需要在setup内执行
+import type { ProjectData } from '@/apis/project'
+import html2canvas from 'html2canvas'
+import { ref, nextTick, computed, onMounted, watch } from 'vue'
+import { UIIcon } from '@/components/ui'
+import logo from './logos/XBuilderLogo.svg'
+import PosterBackground from './postBackground.jpg'
+import { universalUrlToWebUrl } from '@/models/common/cloud'
+import { useExternalUrl } from '@/utils/utils'
+import QRCode from 'qrcode'
+
+const props = defineProps<{
+  img?: File
+  projectData: ProjectData
+}>()
+
+const createPoster = async (): Promise<File> => {
+
+}
+
+defineExpose({
+  createPoster
+})
+</script>
+
+<template>
+</template>
+
+<style scoped lang="scss">
+</style>
