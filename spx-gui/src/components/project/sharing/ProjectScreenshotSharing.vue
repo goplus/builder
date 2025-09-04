59daa7403fbe6fca107e4dcaf4f053741c6b937c
WJH <3506456886@qq.com>
2025-09-04 14:43:42 +0800

    feat: add screenshotSharing

---
+++ spx-gui/src/components/project/sharing/ProjectScreenshotSharing.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, computed, defineProps, defineEmits, watch, nextTick, onUnmounted } from 'vue'
+import Poster from './ProjectPoster.vue'
+import PlatformSelector from './platformSelector.vue'
+import type { ProjectData } from '@/apis/project'
+import type { PlatformConfig } from './platformShare'
+import QRCode from 'qrcode'
+
+const props = defineProps<{
+    screenshot: File
+    projectData: ProjectData
+    visible: boolean
+}>()
+
+const emit = defineEmits<{
+    cancelled: []
+    resolved: [platfrom: string]
+}>()
+
+</script>
+
+<template>
+</template>
+
+<style lang="scss" scoped>
+</style>
