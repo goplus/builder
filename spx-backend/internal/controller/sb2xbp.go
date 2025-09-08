89f96a0f881a6d998336cd67be87ae17985f90a7
wuxinyi <wuxinyi@qiniu.com>
2025-09-08 14:56:16 +0800

    refactor: move Convert function logic into Controller and remove sb2xbp package

--- spx-backend/internal/controller/sb2xbp.go
+++ spx-backend/internal/controller/sb2xbp.go
@@ -3,7 +3,14 @@ package controller
 import (
 	"context"
 
-	"github.com/goplus/builder/spx-backend/internal/sb2xbp"
+	gotypes "go/types"
+
+	"github.com/goplus/sb2xbp/convert"
+	"github.com/goplus/sb2xbp/sbio"
+	"github.com/goplus/sb2xbp/sbio/ast"
+	"github.com/goplus/sb2xbp/sbtypinf"
+	"github.com/goplus/sb2xbp/types"
+	"github.com/goplus/sb2xbp/xbpio"
 )
 
 // Sb2xbpParams defines the parameters for the Scratch to XBP conversion.
@@ -36,13 +43,50 @@ func (p *Sb2xbpParams) Validate() (ok bool, msg string) {
 
 // ConvertXBP converts a file from Scratch format to XBP format.
 func (ctrl *Controller) Convert(ctx context.Context, params *Sb2xbpParams) (result *XBPResult, err error) {
-	data, err := sb2xbp.Convert(params.File.Data, params.File.Version, params.Mapping)
+	g, err := sbio.OpenFromBytes(params.File.Data, params.File.Version)
+	if err != nil {
+		return nil, err
+	}
+
+	varentType := types.Variant
+	varentSlice := types.List
+	info := &sbtypinf.TypeInfo{
+		VarDefs:  make(map[sbio.Variable]gotypes.Type),
+		ListDefs: make(map[sbio.List]gotypes.Type),
+		FuncDefs: make(map[ast.Def]*gotypes.Signature),
+	}
+	err = sbtypinf.Do("main", g, &sbtypinf.Config{
+		TyValue: varentType,
+		TyList:  varentSlice,
+	}, info)
+	if err != nil {
+		return nil, err
+	}
+
+	w := xbpio.New(xbpio.Config{
+		Run: xbpio.RunConfig{
+			Width:  480,
+			Height: 360,
+		},
+	})
+	err = convert.Convert(g, w, convert.Config{
+		Query: info,
+		NameConv: func(s string) string {
+			to, ok := params.Mapping[s]
+			if ok {
+				return to
+			}
+			return s
+		},
+	})
+
+	xbpdata, err := w.Bytes()
 	if err != nil {
 		return nil, err
 	}
 
 	return &XBPResult{File: File{
-		Data:    data,
+		Data:    xbpdata,
 		Version: 1,
 	}}, nil
 }
