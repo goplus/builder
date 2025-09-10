42fa860dd881fcfbc5180d2eced31a30bde0a074
wuxinyi <wuxinyi@qiniu.com>
2025-09-10 14:54:26 +0800

    fix(authenticator): update TODO comment for avatar URL handling in OAuth

--- spx-backend/internal/authn/casdoor/casdoor.go
+++ spx-backend/internal/authn/casdoor/casdoor.go
@@ -59,13 +59,11 @@ func (a *authenticator) Authenticate(ctx context.Context, token string) (*model.
 	mUser, err := model.FirstOrCreateUser(ctx, a.db, model.CreateUserAttrs{
 		Username:    claims.Name,
 		DisplayName: claims.DisplayName,
-		/*
-			TODO(wyvern): https://github.com/goplus/builder/issues/2159
-			 The avatar URL for the three-party oauth authorization may be http,
-			 which will cause the browser to not be able to obtain the avatar.
-			 Currently, it is temporarily replaced with https. A better way is to
-			 download it to our kodo and then obtain it from kodo. Kodo defaults to https.
-		*/
+		// TODO(wyvern): https://github.com/goplus/builder/issues/2159
+		//  The avatar URL for the three-party oauth authorization may be http,
+		//  which will cause the browser to not be able to obtain the avatar.
+		//  Currently, it is temporarily replaced with https. A better way is to
+		//  download it to our kodo and then obtain it from kodo. Kodo defaults to https.
 		Avatar: fixAvatar(claims.Avatar),
 		Roles:  nil,
 		Plan:   model.UserPlanFree,
