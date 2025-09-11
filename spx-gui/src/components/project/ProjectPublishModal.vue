42528a0bb3f15ce355a85c4d87bcfb69480e2bab
hwcctn <2738268497@qq.com>
2025-09-11 18:20:37 +0800

    format

--- spx-gui/src/components/project/ProjectPublishModal.vue
+++ spx-gui/src/components/project/ProjectPublishModal.vue
@@ -100,9 +100,13 @@ async function handleEidtKeyboard() {
 </script>
 
 <template>
-  <UIFormModal :radar="{ name: 'Project publish modal', desc: 'Modal for publishing projects' }"
-    :title="$t({ en: `Publish ${project.name}`, zh: `发布 ${project.name}` })" :style="{ width: '560px' }"
-    :visible="props.visible" @update:visible="handleCancel">
+  <UIFormModal
+    :radar="{ name: 'Project publish modal', desc: 'Modal for publishing projects' }"
+    :title="$t({ en: `Publish ${project.name}`, zh: `发布 ${project.name}` })"
+    :style="{ width: '560px' }"
+    :visible="props.visible"
+    @update:visible="handleCancel"
+  >
     <UIForm :form="form" has-success-feedback @submit="handleSubmit.fn">
       <p v-if="project.visibility === Visibility.Private" class="tip">
         {{
@@ -116,9 +120,12 @@ async function handleEidtKeyboard() {
         <UIImg class="thumbnail" :src="thumbnailUrl" :loading="thumbnailUrlLoading" size="contain" />
       </div>
       <UIFormItem :label="$t({ en: 'Release description', zh: '发布内容' })" path="releaseDescription">
-        <UITextInput v-model:value="form.value.releaseDescription"
-          v-radar="{ name: 'Release description input', desc: 'Input field for release description' }" type="textarea"
-          :placeholder="$t({ en: 'What is new in this release?', zh: '这次发布有什么新内容？' })" />
+        <UITextInput
+          v-model:value="form.value.releaseDescription"
+          v-radar="{ name: 'Release description input', desc: 'Input field for release description' }"
+          type="textarea"
+          :placeholder="$t({ en: 'What is new in this release?', zh: '这次发布有什么新内容？' })"
+        />
       </UIFormItem>
       <!-- mobile -->
       <UIFormItem :label="$t({ en: 'Mobile keyboard', zh: '移动端键盘' })">
@@ -146,30 +153,46 @@ async function handleEidtKeyboard() {
         </div>
       </UIFormItem>
       <UIFormItem :label="$t({ en: 'Project description', zh: '项目描述' })" path="projectDescription">
-        <UITextInput ref="aboutProjectInput" v-model:value="form.value.projectDescription"
-          v-radar="{ name: 'Project description input', desc: 'Input field for project description' }" type="textarea"
-          :placeholder="$t({
-            en: 'What is this project about? How did you make it?',
-            zh: '这个项目是关于什么的？你是如何创作的？'
-          })
-            " />
+        <UITextInput
+          ref="aboutProjectInput"
+          v-model:value="form.value.projectDescription"
+          v-radar="{ name: 'Project description input', desc: 'Input field for project description' }"
+          type="textarea"
+          :placeholder="
+            $t({
+              en: 'What is this project about? How did you make it?',
+              zh: '这个项目是关于什么的？你是如何创作的？'
+            })
+          "
+        />
       </UIFormItem>
       <UIFormItem :label="$t({ en: 'Play instructions', zh: '操作说明' })" path="projectInstructions">
-        <UITextInput v-model:value="form.value.projectInstructions"
+        <UITextInput
+          v-model:value="form.value.projectInstructions"
           v-radar="{ name: 'Play instructions input', desc: 'Input field for project play instructions' }"
-          type="textarea" :placeholder="$t({
-            en: 'Tell others how to play in your project',
-            zh: '告诉其他用户在项目中如何操作'
-          })
-            " />
+          type="textarea"
+          :placeholder="
+            $t({
+              en: 'Tell others how to play in your project',
+              zh: '告诉其他用户在项目中如何操作'
+            })
+          "
+        />
       </UIFormItem>
       <footer class="footer">
-        <UIButton v-radar="{ name: 'Cancel button', desc: 'Click to cancel project publishing' }" type="boring"
-          @click="handleCancel">
+        <UIButton
+          v-radar="{ name: 'Cancel button', desc: 'Click to cancel project publishing' }"
+          type="boring"
+          @click="handleCancel"
+        >
           {{ $t({ en: 'Cancel', zh: '取消' }) }}
         </UIButton>
-        <UIButton v-radar="{ name: 'Publish button', desc: 'Click to publish project' }" type="primary"
-          html-type="submit" :loading="handleSubmit.isLoading.value">
+        <UIButton
+          v-radar="{ name: 'Publish button', desc: 'Click to publish project' }"
+          type="primary"
+          html-type="submit"
+          :loading="handleSubmit.isLoading.value"
+        >
           {{ $t({ en: 'Publish', zh: '发布' }) }}
         </UIButton>
       </footer>
