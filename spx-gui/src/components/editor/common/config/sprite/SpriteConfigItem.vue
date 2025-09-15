d1583e34af3c420457caa3599bba1c562bb327cb
Overu <fengjianok@gmail.com>
2025-09-15 20:08:23 +0800

    feat(spx-gui): add Map Editor's SpriteBasicConfig

---
+++ spx-gui/src/components/editor/common/config/sprite/SpriteConfigItem.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { debounce } from 'lodash'
+
+import { type Project } from '@/models/project'
+import { type Sprite } from '@/models/sprite'
+
+export function wrapUpdateHandler<Args extends any[]>(
+  handler: (...args: Args) => unknown,
+  provideSpriteContext: () => { sprite: Sprite; project: Project },
+  withDebounce = true
+): (...args: Args) => void {
+  const { sprite, project } = provideSpriteContext()
+  const sname = sprite.name
+  const action = { name: { en: `Configure sprite ${sname}`, zh: `修改精灵 ${sname} 配置` } }
+  const wrapped = (...args: Args) => project.history.doAction(action, () => handler(...args))
+  return withDebounce ? debounce(wrapped, 300) : wrapped
+}
+
+export default {}
+</script>
+
+<template>
+  <div class="item">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+.item {
+  display: flex;
+  gap: 12px;
+  align-items: center;
+}
+</style>
--- spx-gui/src/components/editor/map-editor/SpriteBasicConfig.vue
+++ spx-gui/src/components/editor/map-editor/SpriteBasicConfig.vue
@@ -11,14 +11,57 @@ SpriteBasicConfig provide editing form for basic sprite properties, including
 import type { Project } from '@/models/project'
 import type { Sprite } from '@/models/sprite'
 
+import SpriteConfigItem from '@/components/editor/common/config/sprite/SpriteConfigItem.vue'
+import SpritePositionSize from '@/components/editor/common/config/sprite/SpritePositionSize.vue'
+import SpriteDirection from '@/components/editor/common/config/sprite/SpriteDirection.vue'
+import SpriteVisible from '@/components/editor/common/config/sprite/SpriteVisible.vue'
+import { UICheckbox } from '@/components/ui'
+import { ref } from 'vue'
+
 defineProps<{
   sprite: Sprite
   project: Project
 }>()
+
+const collision = ref(false)
+const gravity = ref(false)
+
+function handleGravityUpdate(value: boolean) {
+  gravity.value = value
+  collision.value = value ? true : collision.value
+}
 </script>
 
 <template>
-  <div style="height: 200px">Basic Config for {{ sprite.name }}</div>
+  <div class="sprite-basic-config">
+    <SpritePositionSize :sprite="sprite" :project="project" />
+    <SpriteDirection :sprite="sprite" :project="project" />
+    <SpriteVisible :sprite="sprite" :project="project" />
+    <SpriteConfigItem>
+      {{ $t({ en: 'Physics', zh: '物理特性' }) }}:
+      <div class="physics-item">
+        <UICheckbox v-model:checked="collision" :disabled="gravity">
+          <div>{{ $t({ en: 'Collision', zh: '碰撞' }) }}</div>
+        </UICheckbox>
+        <UICheckbox :checked="gravity" @update:checked="handleGravityUpdate">
+          <div>{{ $t({ en: 'Gravity', zh: '重力' }) }}</div>
+        </UICheckbox>
+      </div>
+    </SpriteConfigItem>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.sprite-basic-config {
+  display: flex;
+  padding: 16px;
+  flex-direction: column;
+  gap: var(--ui-gap-middle);
+}
+
+.physics-item {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+</style>
