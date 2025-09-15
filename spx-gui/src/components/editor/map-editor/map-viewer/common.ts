54c2cf866e0c4eac269d9fab998b328c54eb05db
Hanxing Yang <nighca@live.cn>
2025-09-15 16:12:02 +0800

    Merge pull request #2172 from nighca/issue-2147
    `MapViewer` fro Camera

--- spx-gui/src/components/editor/map-editor/map-viewer/common.ts
+++
@@ -1,8 +0,0 @@
-import { Sprite } from '@/models/sprite'
-import type { Widget } from '@/models/widget'
-
-/** Get name which identifies the node, which may be a sprite or a widget */
-export function getNodeId(target: Sprite | Widget) {
-  const type = target instanceof Sprite ? 'sprite' : 'widget'
-  return `${type}:${target.id}`
-}
