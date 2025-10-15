f139b79dfc841a1795149ea7a0e0b57563808182
xgopilot <noreply@goplus.org>
2025-10-15 02:45:29 +0000

    chore: upgrade spx to version v2.0.0-pre.23
    Related: https://github.com/goplus/spx/releases/tag/v2.0.0-pre.23
    
    Generated with [codeagent](https://github.com/qbox/codeagent)
    Co-authored-by: nighca <nighca@users.noreply.github.com>

--- spx-gui/install-spx.sh
+++ spx-gui/install-spx.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Version of spx, keep in sync with the version in `.env`.
-SPX_VERSION=2.0.0-pre.22
+SPX_VERSION=2.0.0-pre.23
 SPX_NAME="spx_${SPX_VERSION}"
 SPX_FILE_NAME="${SPX_NAME}.zip"
 SPX_FILE_URL="https://github.com/goplus/spx/releases/download/v${SPX_VERSION}/spx_web.zip"
