ef75a7ea6d8325e0ec5c53635a6e611a4122b447
Overu <fengjianok@gmail.com>
2025-11-10 20:23:04 +0800

    feat(spx-gui): add course series route page

---
+++ spx-gui/src/pages/tutorials/course-series.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  courseSeriesId: string
+}>()
+</script>
+
+<template>
+  <section class="course-series-page">
+    {{ courseSeriesId }}
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.course-series-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- spx-gui/src/pages/tutorials/index.vue
+++ spx-gui/src/pages/tutorials/index.vue
@@ -5,34 +5,32 @@
 
     <TutorialsBanner />
     <CenteredWrapper class="centered-wrapper">
-      <ListResultWrapper :query-ret="courseSeriesQuery">
+      <ListResultWrapper :query-ret="courseSeriesQuery" :height="436">
         <template #empty>
           <UIEmpty size="extra-large">
             {{ $t({ en: 'No course series available', zh: '没有可用的课程系列' }) }}
           </UIEmpty>
         </template>
-        <ul class="course-series-list">
-          <CourseSeriesItem
-            v-for="courseSeries in courseSeriesQuery.data.value"
-            :key="courseSeries.id"
-            :course-series-id="courseSeries.id"
-            :course-i-ds="courseSeries.courseIDs"
-            :title="courseSeries.title"
-          >
-            <template #default="{ data: courseList }">
-              <!-- a tag are used for: link preview on hover, context menu support, and better accessibility -->
-              <a
-                v-for="course in courseList"
-                :key="course.id"
-                :href="`/course/${courseSeries.id}/${course.id}/start`"
-                @click="handleCourseClick($event, course, courseSeries, courseList)"
-              >
-                <CourseItem :course="course" />
-              </a>
-            </template>
-          </CourseSeriesItem>
-        </ul>
+        <template #default="{ data }">
+          <ul class="course-series-list">
+            <CourseSeriesItem v-for="courseSeries in data.data" :key="courseSeries.id" :course-series="courseSeries">
+              <template #default="{ data: courseList }">
+                <!-- a tag are used for: link preview on hover, context menu support, and better accessibility -->
+                <a
+                  v-for="course in courseList"
+                  :key="course.id"
+                  :href="`/course/${courseSeries.id}/${course.id}/start`"
+                  @click="handleCourseClick($event, course, courseSeries, courseList)"
+                >
+                  <CourseItem :course="course" />
+                </a>
+              </template>
+            </CourseSeriesItem>
+          </ul>
+        </template>
       </ListResultWrapper>
+
+      <UIPagination v-show="pageTotal > 1" v-model:current="page" class="pagination" :total="pageTotal" />
     </CenteredWrapper>
 
     <CommunityFooter />
@@ -40,13 +38,15 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import { useQuery } from '@/utils/query'
 import { usePageTitle } from '@/utils/utils'
 
-import { type CourseSeries, listAllCourseSeriesByOrder } from '@/apis/course-series'
+import { type CourseSeries, listCourseSeries } from '@/apis/course-series'
 import type { Course } from '@/apis/course'
 
-import { UIEmpty } from '@/components/ui'
+import { UIEmpty, UIPagination } from '@/components/ui'
 import CommunityNavbar from '@/components/community/CommunityNavbar.vue'
 import CourseSeriesItem from '@/components/tutorials/CourseSeriesItem.vue'
 import CenteredWrapper from '@/components/community/CenteredWrapper.vue'
@@ -55,6 +55,8 @@ import CourseItem from '@/components/tutorials/CourseItem.vue'
 import CommunityFooter from '@/components/community/footer/CommunityFooter.vue'
 import TutorialsBanner from '@/components/tutorials/TutorialsBanner.vue'
 import { useTutorial } from '@/components/tutorials/tutorial'
+import { ownerAll } from '@/apis/common'
+import { useRouteQueryParamInt } from '@/utils/route'
 
 usePageTitle({
   en: 'Tutorials',
@@ -63,10 +65,26 @@ usePageTitle({
 
 const tutorial = useTutorial()
 
-const courseSeriesQuery = useQuery((ctx) => listAllCourseSeriesByOrder(ctx.signal), {
-  en: 'Failed to load course series',
-  zh: '加载课程系列失败'
-})
+const page = useRouteQueryParamInt('p', 1)
+const courseSeriesQuery = useQuery(
+  (ctx) =>
+    listCourseSeries(
+      {
+        owner: ownerAll,
+        pageIndex: page.value,
+        pageSize,
+        orderBy: 'order',
+        sortOrder: 'asc'
+      },
+      ctx.signal
+    ),
+  {
+    en: 'Failed to load course series',
+    zh: '加载课程系列失败'
+  }
+)
+const pageSize = 5
+const pageTotal = computed(() => Math.ceil((courseSeriesQuery.data.value?.total ?? 0) / pageSize))
 
 function handleCourseClick(event: MouseEvent, course: Course, courseSeries: CourseSeries, courseList: Course[]) {
   if (event.button !== 0 || event.ctrlKey || event.metaKey || event.shiftKey) {
@@ -98,8 +116,15 @@ function handleCourseClick(event: MouseEvent, course: Course, courseSeries: Cour
 
     .course-series-list {
       display: flex;
-      flex-direction: column;
-      gap: 32px;
+      flex-direction: row;
+      flex-wrap: wrap;
+      gap: 8px;
+      height: 436px;
+    }
+
+    .pagination {
+      align-self: center;
+      margin-top: 8px;
     }
 
     &:not(:has(.course-series-list)) {
