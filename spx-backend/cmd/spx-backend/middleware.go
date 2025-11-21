8ad893dbfdeffdb40ab0a93bf6510676ed304947
Hanxing Yang <nighca@live.cn>
2025-11-21 17:21:08 +0800

    Remove CORSMiddleware (#2490)
    * Remove CORSMiddleware
    
    * Update CORS config in nginx config

--- spx-backend/cmd/spx-backend/middleware.go
+++ spx-backend/cmd/spx-backend/middleware.go
@@ -3,16 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/getsentry/sentry-go"
 	"github.com/qiniu/x/reqid"
-)
-
-var (
-	// corsAllowedHeaders is a pre-computed string for CORS Access-Control-Allow-Headers.
-	corsAllowedHeaders = fmt.Sprintf("Accept, Accept-Encoding, Content-Type, Content-Length, Authorization, X-CSRF-Token, %s, %s",
-		sentry.SentryTraceHeader, sentry.SentryBaggageHeader)
 )
 
 // NewReqIDMiddleware creates a new request ID middleware.
@@ -21,29 +14,6 @@ func NewReqIDMiddleware() func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := reqid.NewContextWith(r.Context(), w, r)
 			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
-}
-
-// NewCORSMiddleware creates a new CORS middleware.
-func NewCORSMiddleware() func(next http.Handler) http.Handler {
-	allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
-	if allowedOrigin == "" {
-		allowedOrigin = "*" // Default to allow all origins.
-	}
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
-			w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, HEAD, POST, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Headers", corsAllowedHeaders)
-
-			// Handle preflight requests.
-			if r.Method == "OPTIONS" {
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-
-			next.ServeHTTP(w, r)
 		})
 	}
 }
