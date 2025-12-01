952b6d6e37f41906af0b781fda0804281cea5e83
nighca <nighca@live.cn>
2025-12-01 09:50:16 +0800

    Use `untilTaskScheduled` instead of `requestIdleCallback`

--- spx-gui/src/polyfills.ts
+++ spx-gui/src/polyfills.ts
@@ -1,24 +1,3 @@
 /**
  * @file Runtime polyfills
  */
-
-// Poly fill for `requestIdleCallback`, which is not supported in Safari.
-// Copied from https://github.com/pladaria/requestidlecallback-polyfill.
-window.requestIdleCallback =
-  window.requestIdleCallback ||
-  function (cb) {
-    const start = Date.now()
-    return setTimeout(function () {
-      cb({
-        didTimeout: false,
-        timeRemaining: function () {
-          return Math.max(0, 50 - (Date.now() - start))
-        }
-      })
-    }, 1)
-  }
-window.cancelIdleCallback =
-  window.cancelIdleCallback ||
-  function (id) {
-    clearTimeout(id)
-  }
