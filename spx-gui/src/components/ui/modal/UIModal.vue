2b4e587ff78a127f8da94f98b9ce3e91e82052d3
Overu <fengjianok@gmail.com>
2026-01-12 21:02:07 +0800

    feat(spx-gui): add custom transform-origin in UIModal

--- spx-gui/src/components/ui/modal/UIModal.vue
+++ spx-gui/src/components/ui/modal/UIModal.vue
@@ -1,13 +1,17 @@
 <template>
   <NModal
+    class="ui-modal"
     :to="attachTo"
     :show="visible"
     :auto-focus="autoFocus"
     :trap-focus="false"
     :mask-closable="maskClosable"
+    :transform-origin="isString(transformOrigin) ? transformOrigin : undefined"
+    :class="{ 'has-custom-origin': !!customTransformOrigin }"
     @update:show="handleUpdateShow"
   >
     <div
+      ref="containerRef"
       v-radar="radar ?? { name: 'Modal', desc: 'A modal dialog for specific purpose' }"
       :class="['container', `size-${size || 'medium'}`]"
     >
@@ -17,13 +21,15 @@
 </template>
 
 <script setup lang="ts">
+import { ref, computed, watchEffect } from 'vue'
+import { isString } from 'lodash'
 import { NModal } from 'naive-ui'
 import type { RadarNodeMeta } from '@/utils/radar'
 import { useModalContainer } from '../utils'
 
 export type ModalSize = 'small' | 'medium' | 'large' | 'full'
 
-defineProps<{
+const props = defineProps<{
   size?: ModalSize
   visible?: boolean
   autoFocus?: boolean
@@ -34,6 +40,7 @@ defineProps<{
    * TODO: Update implementation of `UIModal` to support using `v-radar` directly.
    */
   radar?: RadarNodeMeta
+  transformOrigin?: InstanceType<typeof NModal>['transformOrigin'] | { x: number; y: number }
 }>()
 
 const emit = defineEmits<{
@@ -45,6 +52,27 @@ const handleUpdateShow = (visible: boolean) => {
 }
 
 const attachTo = useModalContainer()
+
+const containerRef = ref<HTMLElement | null>(null)
+const customTransformOrigin = computed(() => {
+  if (containerRef.value == null || props.transformOrigin == null || isString(props.transformOrigin)) return null
+  const { x, y } = props.transformOrigin
+  return `${x - containerRef.value.offsetLeft}px ${y - containerRef.value.offsetTop}px`
+})
+
+const modalElRef = ref<HTMLElement | null>(null)
+watchEffect(() => {
+  if (containerRef.value == null) return
+  let modalEl = modalElRef.value
+  // Cannot get the modal element directly through ref, temporarily obtained via selector
+  if (modalEl == null) {
+    modalEl = modalElRef.value = containerRef.value.closest('.ui-modal')
+  }
+
+  if (customTransformOrigin.value != null && modalEl != null) {
+    modalEl.style.setProperty('--ui-modal-custom-origin', customTransformOrigin.value)
+  }
+})
 </script>
 
 <style lang="scss" scoped>
@@ -72,5 +100,18 @@ const attachTo = useModalContainer()
 .size-full {
   width: 100%;
   margin: 16px;
+}
+</style>
+
+<style lang="scss">
+.ui-modal.has-custom-origin {
+  transform-origin: var(--ui-modal-custom-origin, center) !important;
+
+  &.fade-in-scale-up-transition-enter-active,
+  &.fade-in-scale-up-transition-leave-active,
+  &.fade-in-scale-up-transition-enter-to,
+  &.fade-in-scale-up-transition-leave-to {
+    transform-origin: var(--ui-modal-custom-origin, center) !important;
+  }
 }
 </style>
