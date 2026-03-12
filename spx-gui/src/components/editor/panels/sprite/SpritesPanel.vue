3d067b71c3d665d369b918cbd6ec9d8cd9459860
chennan <chennan@qiniu.com>
2026-03-12 14:49:22 +0800

    remove legacy components

--- spx-gui/src/components/editor/panels/sprite/SpritesPanel.vue
+++ spx-gui/src/components/editor/panels/sprite/SpritesPanel.vue
@@ -1,56 +1,44 @@
 <template>
-  <CommonPanel
-    v-radar="{ name: 'Sprites panel', desc: 'Panel for managing project sprites' }"
-    :expanded="true"
-    :active="selectedSprite != null"
-    :title="$t({ en: 'Sprites', zh: '精灵' })"
-    color="sprite"
-  >
-    <template #add-options>
-      <UIMenu>
-        <UIMenuItem
-          v-radar="{ name: 'Add from local file', desc: 'Click to add sprite from local file' }"
-          @click="handleAddFromLocalFile"
-          >{{ $t({ en: 'Select local file', zh: '选择本地文件' }) }}</UIMenuItem
-        >
-        <UIMenuItem
-          v-radar="{ name: 'Add from asset library', desc: 'Click to add sprite from asset library' }"
-          @click="handleAddFromAssetLibrary"
-          >{{ $t({ en: 'Choose from asset library', zh: '从素材库选择' }) }}</UIMenuItem
-        >
-      </UIMenu>
-    </template>
-    <template #details>
+  <div class="common-panel" :style="cssVars">
+    <section v-radar="{ name: 'Sprites panel', desc: 'Panel for managing project sprites' }" class="details">
+      <PanelHeader :active="selectedSprite != null">
+        {{ $t({ en: 'Sprites', zh: '精灵' }) }}
+        <template #add-options>
+          <UIMenu>
+            <UIMenuItem
+              v-radar="{ name: 'Add from local file', desc: 'Click to add sprite from local file' }"
+              @click="handleAddFromLocalFile"
+            >
+              {{ $t({ en: 'Select local file', zh: '选择本地文件' }) }}
+            </UIMenuItem>
+            <UIMenuItem
+              v-radar="{ name: 'Add from asset library', desc: 'Click to add sprite from asset library' }"
+              @click="handleAddFromAssetLibrary"
+            >
+              {{ $t({ en: 'Choose from asset library', zh: '从素材库选择' }) }}
+            </UIMenuItem>
+          </UIMenu>
+        </template>
+      </PanelHeader>
       <SpriteList />
-    </template>
-    <template #summary>
-      <PanelSummaryList ref="summaryList" :has-more="summaryListData.hasMore">
-        <UIEmpty v-if="sprites.length === 0" size="small">
-          {{ $t({ en: 'Empty', zh: '无' }) }}
-        </UIEmpty>
-        <SpriteSummaryItem v-for="sprite in summaryListData.list" :key="sprite.id" :sprite="sprite" />
-      </PanelSummaryList>
-    </template>
-  </CommonPanel>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { AssetType } from '@/apis/asset'
 import { useMessageHandle } from '@/utils/exception'
 import { useAddAssetFromLibrary, useAddSpriteFromLocalFile } from '@/components/asset'
 import { useEditorCtx } from '@/components/editor/EditorContextProvider.vue'
-import { UIMenu, UIMenuItem, UIEmpty } from '@/components/ui'
+import { UIMenu, UIMenuItem, useUIVariables, getCssVars } from '@/components/ui'
 import SpriteList from '@/components/editor/sprite/SpriteList.vue'
-import CommonPanel from '../common/CommonPanel.vue'
-import PanelSummaryList, { useSummaryList } from '../common/PanelSummaryList.vue'
-import SpriteSummaryItem from './SpriteSummaryItem.vue'
+import PanelHeader from '../common/PanelHeader.vue'
+
+const uiVariables = useUIVariables()
+const cssVars = getCssVars('--panel-color-', uiVariables.color.sprite)
 
 const editorCtx = useEditorCtx()
-
-const sprites = computed(() => editorCtx.project.sprites)
-const summaryList = ref<InstanceType<typeof PanelSummaryList>>()
-const summaryListData = useSummaryList(sprites, () => summaryList.value?.listWrapper ?? null)
 
 const selectedSprite = computed(() => editorCtx.state.selectedSprite)
 
@@ -80,4 +68,17 @@ const handleAddFromAssetLibrary = useMessageHandle(
 ).fn
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.common-panel {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.details {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+</style>
