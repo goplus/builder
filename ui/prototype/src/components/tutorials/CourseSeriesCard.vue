df7ac546e51cdf8492e6ef0de31038d8db670514
qingqing-ux <xxin31516@gmail.com>
2026-05-21 00:28:40 +0800

    Align tutorial card text styling

--- ui/prototype/src/components/tutorials/CourseSeriesCard.vue
+++ ui/prototype/src/components/tutorials/CourseSeriesCard.vue
@@ -15,9 +15,9 @@ defineProps<{
       <div class="h-43.5 w-58 overflow-hidden border-b border-grey-400 bg-grey-300" aria-hidden="true">
         <img class="block size-full object-cover" :src="series.cover" alt="" />
       </div>
-      <div class="flex h-19.5 flex-col gap-1 bg-grey-100 p-4">
-        <h2 class="overflow-hidden text-base leading-5.5 font-normal whitespace-nowrap text-title text-ellipsis">{{ series.title }}</h2>
-        <div class="flex items-center gap-3 text-sm leading-5 text-hint-1">
+      <div class="h-19.5 w-full flex-none overflow-hidden bg-grey-100 p-4">
+        <div class="w-full overflow-hidden text-lg whitespace-nowrap text-title text-ellipsis" :title="series.title">{{ series.title }}</div>
+        <div class="mt-1 inline-flex items-center gap-3 text-sm text-hint-2">
           <span>{{ series.total }}</span>
           <span>{{ series.updatedAt }}</span>
         </div>
