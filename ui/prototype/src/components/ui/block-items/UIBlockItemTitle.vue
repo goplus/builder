d7a0c6690c7e6a5cb79557a4e45d821b066b8b3e
qingqing-ux <xxin31516@gmail.com>
2026-05-22 15:26:01 +0800

    Make medium sprite titles use full width

--- ui/prototype/src/components/ui/block-items/UIBlockItemTitle.vue
+++ ui/prototype/src/components/ui/block-items/UIBlockItemTitle.vue
@@ -1,3 +1,12 @@
+<template>
+  <div :class="rootClass">
+    <span class="min-w-0 flex-1 overflow-hidden text-ellipsis whitespace-nowrap" :title="title">
+      <slot></slot>
+    </span>
+    <slot v-if="slots.suffix != null" name="suffix"></slot>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { computed, useSlots } from 'vue'
 
@@ -9,23 +18,10 @@ const props = defineProps<{
 
 const slots = useSlots()
 const rootClass = computed(() => [
-  'flex items-center text-center text-title',
+  'flex items-center text-title',
   props.size === 'large'
-    ? 'h-5 w-full gap-2 px-1.5 text-sm'
-    : ['h-5.5 w-[76px] gap-0.5 text-2xs', slots.suffix != null ? 'px-0' : 'px-1.5'],
+    ? 'h-5 w-full gap-2 px-1.5 text-center text-sm'
+    : ['h-5.5 w-[76px] gap-0.5 px-0 text-2xs', slots.suffix != null ? 'text-left' : 'text-center'],
   props.class
 ])
 </script>
-
-<template>
-  <div :class="rootClass">
-    <span
-      class="min-w-0 flex-1 overflow-hidden text-ellipsis whitespace-nowrap"
-      :class="{ 'text-left': slots.suffix != null }"
-      :title="title"
-    >
-      <slot></slot>
-    </span>
-    <slot v-if="slots.suffix != null" name="suffix"></slot>
-  </div>
-</template>
