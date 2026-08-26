19050044528a91f49b328a6aee17a3fa68325c79
Ethanlita <lita@ethanlita.com>
2026-08-26 17:40:36 +0800

    fix: align tutorial opening step props

--- spx-gui/src/components/tutorials/TutorialRoot.vue
+++ spx-gui/src/components/tutorials/TutorialRoot.vue
@@ -99,8 +99,8 @@ const currentOpeningStep = computed(() =>
     : null
 )
 const currentOpeningPosition = computed(() => ({
-  index: openingIndexRef.value,
-  count: openingStepsRef.value.length
+  stepIndex: openingIndexRef.value,
+  stepCount: openingStepsRef.value.length
 }))
 
 /**
@@ -410,8 +410,8 @@ provideTutorial(tutorial)
     v-else-if="currentOpeningStep?.kind === 'prelude'"
     visible
     :text="currentOpeningStep.text"
-    :step-index="currentOpeningPosition.index"
-    :step-count="currentOpeningPosition.count"
+    :step-index="currentOpeningPosition.stepIndex"
+    :step-count="currentOpeningPosition.stepCount"
     @continue="advanceOpening()"
   />
   <ApiVideoModal
